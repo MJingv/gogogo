fbc6fd7a37fecf86ee622db26844ddb3d3373149
MJingv <625619954@qq.com>
2021-07-12 20:51:25 +0800

    (feature)+递归memo

--- 递归.js
+++ 递归.js
@@ -17,3 +17,22 @@ const fn1 = (n, sum = 1) => {
 }
 res = fn1(5)//120
 console.log(res)
+
+
+//********* 1.斐波那契 *********
+const fn2 = (n) => {
+    if (n === 0) return 0
+    if (n === 1 || n === 2) return 1
+    return fn2(n - 1) + fn2(n - 2)
+}
+res = fn2(9)
+console.log(res, 'fn2')
+
+//优化--记忆memo
+const list = [0, 1, 1]
+const fn3 = (n) => {
+    if (list[n]) return list[n]
+    return fn3(n - 1) + fn3(n - 2)
+}
+res = fn3(9)
+console.log(res, 'fn3')
