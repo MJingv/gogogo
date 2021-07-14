4879a4fd76d3a625dc8d48c5d9450389ae216a14
MJingv <625619954@qq.com>
2021-07-14 21:04:37 +0800

    (feature)+tree:remove

--- tree.js
+++ tree.js
@@ -74,6 +74,50 @@ class BinarySearchTree {
             return false
         }
     }
+
+    remove(key) {
+        this.root = this.removeNode(this.root, key)
+    }
+
+    removeNode(node, key) {
+        if (!node) {
+            return null
+        }
+        //递归找到要删除的节点
+        if (node.key < key) {
+            node.right = this.removeNode(node.right, key)
+            return node
+        }
+        if (node.key > key) {
+            node.left = this.removeNode(node.left, key)
+            return node
+        }
+        if (node.key === key) {
+            //case1:node是叶子节点
+            if (!node.left && !node.right) {
+                node = null
+                return node
+            }
+            //case2:node有2个节点：用右边最小节点替换node
+            if (node.left && node.right) {
+                let aux = this.minNode(node.right)
+                node.key = aux.key
+                node.right = this.removeNode(node.right, aux.key)
+                return node
+            }
+            //case3:node只有left子节点
+            if (!node.right) {
+                node = node.left
+                this.removeNode(node, key)
+                return node
+            }
+            //case4:node只有right子节点
+            if (!node.left) {
+                node = node.right
+                return node
+            }
+        }
+    }
 }
 
 const t = new BinarySearchTree()
@@ -81,13 +125,12 @@ t.insert(5)
 t.insert(1)
 t.insert(9)
 t.insert(7)
-t.insert(0)
-t.insert(11)
 
 const printFn = (val) => console.log(val)
 // t.inOrderTraverse(printFn)
 // t.min()
 // t.max()
-const res = t.search(0)
-
+// t.search(0)
+t.remove(9)
+const res = JSON.stringify(t)
 console.log(res)
