f61d64ef13fa8a1b576f5259f7dc2900ff3738e0
MJingv <625619954@qq.com>
2021-07-15 20:03:54 +0800

    (feature)+stack:while

--- stack.js
+++ stack.js
@@ -8,5 +8,19 @@ const fn = (n) => {
     if (Math.floor(n / 2) === 0) return list.reverse().join('')
     fn(a)
 }
-fn(10)
-console.log(list.reverse().join(''))
+// fn(10)
+// console.log(list.reverse().join(''))
+
+//循环实现
+const fn2 = (n) => {
+    while (n > 0) {
+        let a = Math.floor(n / 2)
+        let b = n % 2
+        list.push(b)
+        n = a
+    }
+    return list
+}
+
+const res = fn2(11)
+console.log(res.reverse().join(''))
