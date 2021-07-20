777043d1a5f7965d6a4f4e4ae06b0e1ed24b4290
MJingv <625619954@qq.com>
2021-07-20 15:18:47 +0800

    (feature)+insert

--- doubleLink.js
+++ doubleLink.js
@@ -15,26 +15,47 @@ class DoublyLinkList extends LinkList {
         this.tail = undefined
     }
 
-    insert(element, index) {
-        if (index < 0 || index > this.count - 1) return false
-        const node = new DoublyNode(element)
-        let current = this.head
-        if (index === 0) {
-            if (!this.head && !this.tail) {
-                this.head = this.tail = node
-            } else {
-                node.next = this.head
-                this.head.prev = node
-                this.head = node
-            }
-        } else {
+    insert(element, index = 0) {
 
+        if (index >= 0 && index <= this.count) {
+            const node = new DoublyNode(element)
+            let current = this.head
+            if (index === 0) {
+                //第一个，链表为null
+                if (!this.head) {
+                    this.head = node
+                    this.tail = node
+                } else {
+                    node.next = this.head
+                    current.prev = node
+                    this.head = node
+                }
+            } else if (index === this.count) {
+                //最后一个
+                current = this.tail
+                current.next = node
+                node.prev = current
+                this.tail = node
+
+            } else {
+                const aux = this.getEelementAt(index - 1)
+                current = aux.next
+                node.prev = aux
+                node.next = current
+                aux.next = node
+                current.prev = node
+            }
+            this.count++
+            return true
+        } else {
+            return false
         }
     }
-
 }
 
 const l = new DoublyLinkList()
 l.insert(5)
-const res = JSON.stringify(l)
-console.log(res)
+l.insert(11, 1)
+l.insert(99, 1)
+
+console.log(l)
