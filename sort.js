db0d38ece15a1bbf61e2ca40108a0e1c7920d1a4
MJingv <625619954@qq.com>
2021-07-22 16:26:18 +0800

    (feature)+选择排序

--- sort.js
+++ sort.js
@@ -1,4 +1,4 @@
-//冒泡排序
+//冒泡排序 O(n^2)
 const swap = (array, a, b) => {
     [array[a], array[b]] = [array[b], array[a]]
 }
@@ -12,6 +12,22 @@ const fn = (array) => {
     return array
 }
 
-const list = [5, 4, 3, 2]
-const res = fn(list)
+//选择排序 O(n^2)
+const fn1 = (array) => {
+    if (!array || !array.length) return
+    for (let i = 0; i < array.length - 1; i++) {
+        let tmpIndex = i
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[tmpIndex] > array[j]) {
+                tmpIndex = j
+            }
+        }
+        i !== tmpIndex && swap(array, i, tmpIndex)
+    }
+    return array
+}
+
+const list = [5, 1, 4, 3, 2]
+const res = fn1(list)
 console.log(res)
+
