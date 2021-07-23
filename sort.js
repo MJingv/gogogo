e4f89d4e5536795e3198b3e4166f03302645b546
MJingv <625619954@qq.com>
2021-07-23 16:32:58 +0800

    (feature)+归并排序（我好蠢）

--- sort.js
+++ sort.js
@@ -1,8 +1,8 @@
 //冒泡排序 O(n^2)
 //比我大/小就交换
-const swap = (array, a, b) => {
-    [array[a], array[b]] = [array[b], array[a]]
-}
+const swap = (array, a, b) => [array[a], array[b]] = [array[b], array[a]]
+const sum = (array) => array.reduce((total, curVal) => total + curVal, 0)
+
 const fn = (array) => {
     if (!array || !array.length) return
     for (let i = 0; i < array.length; i++) {
@@ -44,8 +44,31 @@ const fn2 = (array) => {
     return array
 }
 
+//归并排序
+//O(nlog^n)
+const merge = (left = [], right = []) => {
+    let res = []
+    let i = 0
+    let j = 0
+    while (i < left.length && j < right.length) {
+        res.push(left[i] < right[j] ? left[i++] : right[j++])
+    }
+    return res.concat(i < left.length ? left.slice(i) : right.slice(j))
+}
+
+
+const fn3 = (array) => {
+    if (array.length > 1) {
+        const middle = Math.floor(array.length / 2)
+        const left = fn3(array.slice(0, middle))
+        const right = fn3(array.slice(middle))
+        array = merge(left, right)
+    }
+    return array
+}
+
 
 const list = [3, 5, 1, 4, 2]
-const res = fn2(list)
+const res = fn3(list)
 console.log(res)
 
