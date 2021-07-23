69fbaca2c080c30a7b6cca24eea7924840cbac23
MJingv <625619954@qq.com>
2021-07-23 11:14:05 +0800

    (feature)+插入排序v1

--- sort.js
+++ sort.js
@@ -1,4 +1,5 @@
 //冒泡排序 O(n^2)
+//比我大/小就交换
 const swap = (array, a, b) => {
     [array[a], array[b]] = [array[b], array[a]]
 }
@@ -13,6 +14,7 @@ const fn = (array) => {
 }
 
 //选择排序 O(n^2)
+//选最大/小的放前面
 const fn1 = (array) => {
     if (!array || !array.length) return
     for (let i = 0; i < array.length - 1; i++) {
@@ -26,8 +28,24 @@ const fn1 = (array) => {
     }
     return array
 }
+//插入排序
+const fn2 = (array) => {
+    if (!array || !array.length) return
+    for (let i = 1; i < array.length; i++) {
+        //分割，i前已排好
+        let val = array[i]
+        let tmpIndex = i
+        for (let j = i - 1; j >= 0; j--) {
+            if (val < array[j]) tmpIndex = j
+        }
+        array.splice(i, 1)
+        array.splice(tmpIndex, 0, val)
+    }
+    return array
+}
 
-const list = [5, 1, 4, 3, 2]
-const res = fn1(list)
+
+const list = [3, 5, 1, 4, 2]
+const res = fn2(list)
 console.log(res)
 
