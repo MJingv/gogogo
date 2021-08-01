f12ad491114975059e5b4de6603d1ea0384ae276
MJingv <625619954@qq.com>
2021-08-01 12:48:12 +0800

    (feature)+new

--- handmake.js
+++ handmake.js
@@ -2,7 +2,7 @@
 const throttle = (fn, delay) => {
     let last = null
     return () => {
-        const now = new Date()
+        let now = new Date()
         if (now - last > delay) {
             fn()
             last = now
@@ -14,38 +14,36 @@ const throttle = (fn, delay) => {
 const debounce = (fn, delay) => {
     let timer = null
     return () => {
-        if (timer) clearTimeout(timer)
-        timer = setTimeout(() => {
-            fn()
-        }, delay)
+        clearTimeout(timer)
+        timer = setTimeout(fn, delay)
     }
+
 }
 //手写call apply bind
-
 Function.prototype.mycall = function (ctx, ...res) {
     ctx.foo = this
     ctx.foo(res)
     delete ctx.foo
 }
-
-Function.prototype.mybind = function (ctx, ...res) {
-    ctx.foo = this
-
-    return () => {
-        ctx.foo(res)
-        delete ctx.foo
-    }
-}
-
 const k = {
     name: 'kk'
 }
-const sayname = function () {
-    console.log(this.name, '---myname')
+const getname = function () {
+    console.log(this.name, '----')
 }
-// sayname.mycall(k, 'wo是参数')
-const a = sayname.mybind(k, 'wo是参数')
-a()
-
+// getname.mycall(k, '999')
 
 //手写new
+const myNew = function (Foo, ...res) {
+    const obj = {}
+    obj.__proto__ = Foo.prototype
+    Foo.call(obj, ...res)
+    return obj
+}
+
+function Foo(name) {
+    this.name = name + '999'
+}
+
+const kk = myNew(Foo, 'kkkk')
+console.log(kk.name)
