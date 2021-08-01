e86f82f201bc43f528b1cb94a6a27fe85e06c70d
MJingv <625619954@qq.com>
2021-08-01 12:08:11 +0800

    (feature)+call/apply

--- handmake.js
+++ handmake.js
@@ -20,3 +20,32 @@ const debounce = (fn, delay) => {
         }, delay)
     }
 }
+//手写call apply bind
+
+Function.prototype.mycall = function (ctx, ...res) {
+    ctx.foo = this
+    ctx.foo(res)
+    delete ctx.foo
+}
+
+Function.prototype.mybind = function (ctx, ...res) {
+    ctx.foo = this
+
+    return () => {
+        ctx.foo(res)
+        delete ctx.foo
+    }
+}
+
+const k = {
+    name: 'kk'
+}
+const sayname = function () {
+    console.log(this.name, '---myname')
+}
+// sayname.mycall(k, 'wo是参数')
+const a = sayname.mybind(k, 'wo是参数')
+a()
+
+
+//手写new
