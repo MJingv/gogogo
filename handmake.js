405d2f5ffc94f48cd2cb24a15c184c24052f0475
MJingv <625619954@qq.com>
2021-08-02 15:56:09 +0800

    (feature)+ajax

--- handmake.js
+++ handmake.js
@@ -57,7 +57,6 @@ function Foo(name) {
 // console.log(kk.name)
 
 //继承
-
 function Parent() {
     this.name = 'p'
 
@@ -78,3 +77,33 @@ Child.prototype.constructor = Child
 
 const c = new Child()
 console.log(c.say())
+
+
+const formate = (obj) => {
+    let res = []
+    for (let i in obj) {
+        res.push(`${i}=${obj[i]}`)
+    }
+    return res.join('&')
+
+}
+//promise封装ajax
+const ajax = (type, url, data) => new Promise((res, rej) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open(type, url, true)
+    xhr.onreadystatechange(() => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            res(JSON.parse(xhr.responseText))
+        } else {
+            rej(xhr)
+        }
+    })
+
+    if (type === 'GET') {
+        xhr.send()
+    } else if (type === 'POST') {
+        xhr.setRequestHeader("Content-type", 'application/x-wwww-form-urlencoded')
+        xhr.send(formate(data))
+    }
+
+})
