93740c2be9a814bafb5489a44cf0814fc1a2f01f
MJingv <625619954@qq.com>
2022-03-15 19:38:54 +0800

    (feature)avltree

--- tree.js
+++ tree.js
@@ -442,30 +442,6 @@ const l = t.binaryTreePaths()
 console.log(l)
 
 
-class AVLTree extends BinarySearchTree {
-    constructor(compareFn) {
-        super(compareFn);
-        this.compareFn = comparFn
-        this.root = null
-    }
-
-    getNodeHeight(node) {
-        if (!node) return -1
-        return Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) + 1
-    }
-}
-
-// const t = new AVLTree()
-// t.insert(5)
-// t.insert(1)
-// t.insert(9)
-// t.insert(7)
-// t.insert(11)
-// t.insert(10)
-//
-//
-// const res = t.getNodeHeight(t.root)
-// console.log(res)
 
 
 
