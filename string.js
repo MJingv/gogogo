bde09498f3cd7ad4100a005095c76f7b6d444b8e
MJingv <625619954@qq.com>
2022-08-04 15:12:49 +0800

    字符串全排列+1

--- string.js
+++ string.js
@@ -3,18 +3,20 @@
 // bac bca
 // cab aba
 
+//入参是待排列的string
+//出参是本次的字符串数组
 const fun = (str = '') => {
-    if (!str) return []
-    if (str.length === 1) return [str]
+    if (!str.length) return ['']
+    if (str.length === 1) return [str] //结束条件
     const res = []
-    str.split('').map((item, index) => {
-        const first = item
-        const left = str.slice(0, index) + str.slice(index + 1)//剔除固定的字符
+    for (let i = 0; i < str.length; i++) {
+        const first = str[i]
+        const left = str.slice(0, i) + str.slice(i + 1)
         const next = fun(left)
         next.map(i => {
             res.push(first + i)
         })
-    })
+    }
     return res
 }
 
