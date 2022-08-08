8d71ad85a4155f222098eaefae9d62076a8e45e7
MJingv <625619954@qq.com>
2022-08-08 18:03:47 +0800

    数值的整数次方

--- recursion.js
+++ recursion.js
@@ -17,6 +17,27 @@ const mergeTwoLists = function (list1, list2) {
 const sumNums = (n) => {
     return n && sumNums(n - 1) + n
 }
-const res = sumNums(3)
+// const res = sumNums(3)
 
+
+// 「剑指 Offer 16. 数值的整数次方」
+// 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。不得使用库函数，同时不需要考虑大数问题。
+
+// 输入：x = 2.00000, n = -2
+// 输出：0.25000
+// 解释：2-2 = 1/22 = 1/4 = 0.25
+
+
+const pow = (x, n) => {
+    if (n === 0) return 1
+    if (n < 0) {
+        return 1 / pow(x, -n)
+    }
+    if (n & 1) {//奇数
+        return pow(x, n - 1) * x
+    }
+    //偶数
+    return pow(x * x, n / 2)
+}
+const res = pow(2.00000, 4)
 console.log(res)
