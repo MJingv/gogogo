cbf4c85512392291868bdd856eb6796f4f250cea
MJingv <625619954@qq.com>
2022-08-09 12:02:44 +0800

    礼物的最大价值+1

--- dp.js
+++ dp.js
@@ -58,19 +58,17 @@ const cuttingRope = (n) => {
 const maxValue = (grid) => {
     if (!grid.length) return
     const [m, n] = [grid.length, grid[0].length]
-    const dp = new Array(m).fill(0).map(i => new Array(n).fill(0))
-    dp[0][0] = grid[0][0] //初始化出口
-
+    const dp = Array(m).fill(0).map(i => Array(n).fill(0))
+    dp[0][0] = grid[0][0]
     for (let i = 1; i < m; i++) {
-        dp[i][0] = dp[i - 1][0] + grid[i][0] //第一列
+        dp[i][0] = dp[i - 1][0] + grid[i][0] //第一列，只能向下走
     }
     for (let j = 1; j < n; j++) {
-        dp[0][j] = dp[0][j - 1] + grid[0][j] //第一行
+        dp[0][j] = dp[0][j - 1] + grid[0][j] //第一行，只能向右走
     }
-
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
-            dp[i][j] = Math.max(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j])
+            dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
         }
     }
     return dp[m - 1][n - 1]
