d6d21386c160068dd6b35bbc60d87c3420b36831
MJingv <625619954@qq.com>
2022-08-09 11:10:01 +0800

    礼物的最大价值

--- dp.js
+++ dp.js
@@ -39,6 +39,47 @@ const cuttingRope = (n) => {
     }
     return dp[n]
 }
-const res = cuttingRope(10)
+// const res = cuttingRope(10)
+
+
+// 「剑指 Offer 47. 礼物的最大价值」
+// 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+
+// 输入:
+//     [
+//         [1,3,1],
+//         [1,5,1],
+//         [4,2,1]
+//     ]
+// 输出: 12
+// 解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+
+
+const maxValue = (grid) => {
+    if (!grid.length) return
+    const [m, n] = [grid.length, grid[0].length]
+    const dp = new Array(m).fill(0).map(i => new Array(n).fill(0))
+    dp[0][0] = grid[0][0] //初始化出口
+
+    for (let i = 1; i < m; i++) {
+        dp[i][0] = dp[i - 1][0] + grid[i][0] //第一列
+    }
+    for (let j = 1; j < n; j++) {
+        dp[0][j] = dp[0][j - 1] + grid[0][j] //第一行
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = Math.max(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j])
+        }
+    }
+    return dp[m - 1][n - 1]
+}
+const res = maxValue([
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1]
+])
 
 console.log(res)
+
