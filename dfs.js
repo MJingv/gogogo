3414ef4f1af666515faeb12b862ea8b67b8b3328
MJingv <625619954@qq.com>
2022-08-10 20:27:10 +0800

    dfs.js

---
+++ dfs.js
@@ -0,0 +1,9 @@
+// 「剑指 Offer 12. 矩阵中的路径」
+// 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// 输出：true
+
+const exist = (board, word) => {
+
+}
+const res = exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED")
+console.log(res)
