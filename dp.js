37af519d69967b5e997ea97d44f64b914e1a66cd
MJingv <625619954@qq.com>
2022-08-10 17:14:04 +0800

    window.js

--- dp.js
+++ dp.js
@@ -96,7 +96,29 @@ const maxProfit = (prices) => {
     }
     return dp[dp.length - 1]
 }
-const res = maxProfit([7, 1, 5, 3, 6, 4])
+// const res = maxProfit([7, 1, 5, 3, 6, 4])
 
+
+// 剑指 Offer 46. 把数字翻译成字符串
+// 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+// 输入: 12258
+// 输出: 5
+// 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+
+const translateNum = (num) => {
+    const str = `${num}` //转化成string
+    const dp = [1, 1] //第i个最大可能
+    const len = str.length
+    for (let i = 2; i < len + 1; i++) {
+        const preNum = parseInt(str[i - 2] + str[i - 1]) //之前的数字能不能成组
+        if (preNum >= 10 && preNum <= 25) {
+            dp[i] = dp[i - 1] + dp[i - 2] //能成组，之前分别之和
+        } else {
+            dp[i] = dp[i - 1] //不能成组之前算过，直接拿之前的值
+        }
+    }
+    return dp[dp.length - 1]
+}
+const res = translateNum(12258)
 console.log(res)
 
---
+++ window.js
@@ -0,0 +1,22 @@
+//滑动窗口
+
+// 「剑指 Offer 48. 最长不含重复字符的子字符串」
+// 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+// 输入: "abcabcbb"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+const lengthOfLongestSubstring = (s) => {
+    if (!s.length) return 0
+    let [window, len, max] = [[], s.length, 0]
+    for (let i = 0; i < len; i++) {
+        if (window.includes(s[i])) {
+            max = Math.max(max, window.length)
+            window = []
+        }
+        window.push(s[i])
+    }
+    return max
+}
+const res = lengthOfLongestSubstring('abcabcbb')
+console.log(res)
