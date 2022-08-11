d61b3146985e63138905687e34f77aae70a7aa15
MJingv <625619954@qq.com>
2022-08-11 17:38:10 +0800

    exist

--- dfs.js
+++ dfs.js
@@ -6,25 +6,21 @@
 // 输出：true
 
 const exist = (board, word) => {
-    // if (!board.length || !word) return false
+    if (!board.length || !word) return false
     const [m, n] = [board.length, board[0].length]
-
     const dfs = (i, j, index) => {
-        if (i < 0 || i >= m || j >= n || j < 0 || board[i][j] !== word[index]) return false
-        if (index === word.length - 1) return true
+        if (i < 0 || i >= m || j < 0 || j >= n || word[index] !== board[i][j]) return false //终止条件
+        if (index === word.length - 1) return true //回溯的终点
         const tmp = board[i][j]
+        board[i][j] = '' //防止回溯的时候重复
         console.log(tmp, index)
-        board[i][j] = '' // 将当前字符设置为空，防止四个方向dfs再次遍历到
-        const res = dfs(i - 1, j, index+1) || dfs(i + 1, j, index+1) || dfs(i, j - 1, index+1) || dfs(i, j + 1, index+1)
+        const res = dfs(i - 1, j, index + 1) || dfs(i + 1, j, index + 1) || dfs(i, j - 1, index + 1) || dfs(i, j + 1, index + 1)
         board[i][j] = tmp
         return res
     }
-
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (board[i][j] === word[0]) {
-                if (dfs(i, j, 0)) return true
-            }
+            if (dfs(i, j, 0)) return true
         }
     }
     return false
