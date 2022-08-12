785dc4fb0fe1e71990cc1c18758b6325c89afcf0
MJingv <625619954@qq.com>
2022-08-12 14:05:05 +0800

    200. 岛屿数量

--- dfs.js
+++ dfs.js
@@ -63,5 +63,41 @@ const movingCount = (m, n, k) => {
     dfs(0, 0)
     return res
 }
-const res = movingCount(2, 3, 1)
+// const res = movingCount(2, 3, 1)
+
+
+// 200. 岛屿数量
+// 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。此外，你可以假设该网格的四条边均被水包围。
+
+const numIslands = (grid) => {
+    if (!grid.length) return
+    const [m, n] = [grid.length, grid[0].length]
+    let res = 0
+    const dfs = (i, j) => {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === '0' || grid[i][j] === '2') return false
+        grid[i][j] = '2'
+        dfs(i + 1, j)
+        dfs(i - 1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === '1') {
+                dfs(i, j)
+                if (grid.length) res++
+                // console.log(grid)
+            }
+        }
+    }
+    return res
+
+}
+const res = numIslands([
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "1"]
+])
+
 console.log(res)
