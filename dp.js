46bc1ab180f0d7c81d1c3e0f977794f2d6cc0e0a
MJingv <625619954@qq.com>
2022-08-15 20:07:20 +0800

    42. 接雨水

--- dp.js
+++ dp.js
@@ -177,4 +177,27 @@ const minPathSum = (grid) => {
 // ])
 // console.log(res)
 
+// 42. 接雨水
+// 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+// 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// 输出：6
+// 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
 
+const trap1 = (height) => {
+    if (!height.length) return
+    const res = Array(height.length).fill(0)
+    res[0] = 0
+    for (let i = 1; i < height.length; i++) {
+        const leftMax = Math.max(...height.slice(0, i))
+        const rightMax = Math.max(...height.slice(i + 1))
+        const min = Math.min(leftMax, rightMax)
+        if (height[i] < min) {
+            const diff = Math.abs(min - height[i])
+            res[i] = diff
+        }
+    }
+    const sum = res.reduce((i, p) => i + p)
+    return sum
+}
+const res = trap1([4, 2, 0, 3, 2, 5])
+console.log(res)
