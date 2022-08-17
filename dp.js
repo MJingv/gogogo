00e3a8b1543230f58e88fd7367028f1d314f7bee
MJingv <625619954@qq.com>
2022-08-17 19:25:45 +0800

    findLength2

--- dp.js
+++ dp.js
@@ -392,22 +392,50 @@ const findLength = (nums1, nums2) => {
 
 const findLength2 = (nums1, nums2) => {
     //一维数组dp，保存对角线的值
-    if (!nums1.length || !nums2.length) return 0
-    const [m, n] = [nums1.length, nums2.length]
-    const dp = Array(n).fill(0)
+    if (!nums1.length || !nums2.length) return
+    const dp = []
     let max = 0
+    const [m, n] = [nums1.length, nums2.length]
     for (let i = 0; i < m; i++) {
         for (let j = n - 1; j >= 0; j--) {
             if (nums1[i] === nums2[j]) {
                 dp[j] = (dp[j - 1] || 0) + 1
                 max = Math.max(max, dp[j])
             } else {
-                dp[j] = 0 //我只要右上角的值，不是这一列最大的值（重要）
+                dp[j] = 0
+
             }
         }
     }
-    return dp
+    return max
 }
 
-const res = findLength2([1, 0, 0, 0, 1], [1, 0, 0, 1, 1])
+// const res = findLength2([1, 0, 0, 0, 1], [1, 0, 0, 1, 1])
+// console.log(res)
+
+
+// 300. 最长递增子序列
+// 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+// 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+// 输入：nums = [10,9,2,5,3,7,101,18]
+// 输出：4
+// 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4
+
+const lengthOfLIS = (nums) => {
+    const len = nums.length
+    if (!len) return 0
+    const dp = Array(len).fill(1)
+    // 一定以nums[i]结尾的最长递增子序列 dp[i]=Math.max(nums[i],dp[j]+1)
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1)
+            }
+        }
+    }
+    const max = Math.max(...dp)
+    return max
+}
+const res = lengthOfLIS([0, 1, 0, 3, 2, 3]) //4
 console.log(res)
+
