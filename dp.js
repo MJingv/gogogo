ae6179629c5101249df11edd25ae4d18b105ab0b
MJingv <625619954@qq.com>
2022-08-19 14:41:51 +0800

    打家劫舍2

--- dp.js
+++ dp.js
@@ -508,5 +508,29 @@ const rob = (nums) => {
     }
     return dp[len - 1]
 };
-const res = rob([2, 1])
+// const res = rob([2, 1])
+// console.log(res)
+
+// 213. 打家劫舍 II
+// 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+
+// 输入：nums = [2,3,2,5]
+// 输出：3
+// 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+const rob2 = (nums) => {
+    const len = nums.length
+    if (!len) return 0
+    if (len === 1) return nums[0]
+    const dp1 = [nums[0], Math.max(nums[1], nums[0])]
+    const dp2 = [0, nums[1]]
+    for (let i = 2; i < len; i++) {
+        dp2[i] = Math.max(dp2[i - 2] + nums[i], dp2[i - 1])
+        if (i < len - 1) {
+            dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1])
+        }
+    }
+    return Math.max(dp1[dp1.length - 1], dp2[dp2.length - 1])
+};
+const res = rob2([2, 3, 2, 5])
 console.log(res)
