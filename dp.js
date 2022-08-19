ba2bf9e45f33425009fb9a9563f176795436f5eb
MJingv <625619954@qq.com>
2022-08-19 17:33:15 +0800

    numSquares

--- dp.js
+++ dp.js
@@ -558,6 +558,17 @@ const maxProduct = (nums) => {
     }
     return max
 }
-const res = maxProduct([2, 3, -2, 4])
-console.log(res)
+// const res = maxProduct([2, 3, -2, 4])
+// console.log(res)
 
+// 279. 完全平方数
+// 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+// 输入：n = 12
+// 输出：3
+// 解释：12 = 4 + 4 + 4
+const numSquares = (n) => {
+
+
+}
+const res = numSquares(12)
+console.log(res)
