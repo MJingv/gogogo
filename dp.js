70a448c80d3fc6785d303277b46f3a04f8e01664
MJingv <625619954@qq.com>
2022-08-22 16:03:01 +0800

    coinChange

--- dp.js
+++ dp.js
@@ -579,5 +579,27 @@ const numSquares = (n) => {
 }
 // const res = numSquares(12)
 // console.log(res)
+//
+// 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+// 你可以认为每种硬币的数量是无限的。
+// 示例 1：
+// 输入：coins = [1, 2, 5], amount = 11
+// 输出：3
+// 解释：11 = 5 + 5 + 1
 
-
+const coinChange = (coins, amount) => {
+    const len = coins.length
+    if (!amount || !len) return 0
+    const dp = Array(amount + 1).fill(amount)
+    dp[0] = 0
+    for (let i = 1; i < amount + 1; i++) {
+        for (let j = 0; j < len; j++) {
+            if (i >= coins[j]) {
+                dp[i] = Math.min(dp[i - coins[j]] + 1, dp[i])
+            }
+        }
+    }
+    return dp
+}
+const res = coinChange([ 2, 5], 12)
+console.log(res)
