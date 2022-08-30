c28daea6caceb2e724f96c23c9c784aa1e9455ce
MJingv <625619954@qq.com>
2022-08-30 21:07:06 +0800

    moveZeroes

--- array.js
+++ array.js
@@ -33,5 +33,29 @@ const removeElement = function (nums, val) {
     }
     return nums
 };
-const res = removeElement([3, 2, 2, 3], 2)
+// const res = removeElement([3, 2, 2, 3], 2)
+// console.log(res)
+
+// 第 283 题「 移动零」
+// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+// 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+// 示例 1:
+// 输入: nums = [0,1,0,3,12]
+// 输出:[1,3,12,0,0]
+const moveZeroes = (nums) => {
+    // 记录非0个数，其他补充0
+    const len = nums.length
+    let j = 0
+    for (let i = 0; i < len; i++) {
+        if (nums[i] !== 0) {
+            nums[j] = nums[i]
+            j++
+        }
+    }
+    for (let i = j; i < len; i++) {
+        nums[i] = 0
+    }
+    return nums
+};
+const res = moveZeroes([0, 0, 1])
 console.log(res)
