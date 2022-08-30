77655ca5d89148f4a6880a9b2bca0b51caf55d5f
MJingv <625619954@qq.com>
2022-08-30 20:36:36 +0800

    removeElement

--- array.js
+++ array.js
@@ -15,7 +15,23 @@ const removeDuplicates = (nums) => {
     }
     return slow + 1
 };
-const res = removeDuplicates([1, 2, 3, 4, 4, 5, 6, 6, 7])
-console.log(res)
+// const res = removeDuplicates([1, 2, 3, 4, 4, 5, 6, 6, 7])
+// console.log(res)
 
 // 第 27 题「 移除元素」
+// 输入：nums = [0,1,2,2,3,0,4,2], val = 2 输出：5, nums = [0,1,4,0,3]
+const removeElement = function (nums, val) {
+    const len = nums.length
+    if (!len) return
+    let [fast, slow] = [0, 0]
+    while (fast < len) {
+        if (nums[fast] !== val) {
+            nums[slow] = nums[fast]
+            slow++
+        }
+        fast++
+    }
+    return nums
+};
+const res = removeElement([3, 2, 2, 3], 2)
+console.log(res)
