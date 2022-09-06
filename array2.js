962dffa745082368f247eae5d8cebaeeea8d88fd
MJingv <625619954@qq.com>
2022-09-06 20:50:25 +0800

    304

--- array2.js
+++ array2.js
@@ -13,3 +13,28 @@ var NumArray = function (nums) {
 NumArray.prototype.sumRange = function (left, right) {
     return this.preNum[right + 1] - this.preNum[left]
 };
+// const n = new NumArray([-2, 0, 3, -5, 2, -1])
+// console.log(n.sumRange(0, 2))
+
+// 304. 二维区域和检索 -矩阵不可变
+
+//细节魔鬼，不做了
+var NumMatrix = function (matrix) {
+    const [m, n] = [matrix.length , matrix[0].length ]
+    const preSum = new Array(m+1).fill(0).map(i => new Array(n+1).fill(0))
+    preSum[0][0] = 0
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; i <= n; j++) {
+            preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] + matrix[i - 1][j - 1] - preSum[i - 1][j - 1]
+        }
+    }
+    this.preSum = matrix
+    console.log(preSum)
+};
+
+NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
+    return
+};
+const m = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]])
+const res = m.sumRegion(2, 1, 4, 3)
+console.log(res)
