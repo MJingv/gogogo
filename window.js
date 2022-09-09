665afe456f6897b0e4fff829f8d4642be0e5dfd1
MJingv <625619954@qq.com>
2022-09-09 14:58:31 +0800

    力扣第 76 题「 最小覆盖子串」 hard

--- window.js
+++ window.js
@@ -1,4 +1,6 @@
 //滑动窗口
+// 左开右闭，[0,0) 初始化window里没有数值
+
 
 // 「剑指 Offer 48. 最长不含重复字符的子字符串」
 // 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
@@ -32,7 +34,6 @@ const lengthOfLongestSubstring = (s) => {
 // 输出：3
 // 解释：长度最长的公共子数组是 [3,2,1] 。
 const findLength = (nums1, nums2) => {
-
     if (!nums2.length || !nums1.length) return 0
     const [l1, l2] = [nums1.length, nums2.length]
     const res = []
@@ -53,17 +54,54 @@ const findLength = (nums1, nums2) => {
             }
         } else {
             const a = nums1.slice(i - l1 + 1)
-            const b = nums2.slice(i-l2+1)
+            const b = nums2.slice(i - l2 + 1)
 
 
             //放弃，，，，，
         }
+    }
+    return res
+}
+// const res = findLength([1, 2, 3, 2, 1], [3, 2, 1, 4, 7])
+// console.log(res)
 
-
+// 力扣第 76 题「 最小覆盖子串」 hard
+// 输入：s = "ADOBECODEBANC", t = "ABC" 输出："BANC"
+var minWindow = function (s, t) {
+    if (t.length > s.length) return ''
+    const len = s.length
+    let [left, right, res] = [0, 0, '']
+    const map = new Map()
+    for (let i = 0; i < t.length; i++) {
+        const c = t[i]
+        map.set(c, map.get(c) ? map.get(c) + 1 : 1); //target可能会重复
+    }
+    let mapSize = map.size
+    while (right < len) {
+        // 扩大
+        const c = s[right]
+        right++
+        if (map.has(c)) {
+            map.set(c, map.get(c) - 1)
+            if (map.get(c) === 0) mapSize--
+        }
+        console.log(right, left)
+        while (mapSize === 0) {
+            // 缩小
+            const newStr = s.substring(left, right + 1)
+            if (!newStr || newStr.length < t.length) {
+                res = newStr
+            }
+            const c = s[left]
+            left++
+            if (map.has(c)) {
+                map.set(c, map.get(c) + 1)
+                if (map.get(c) === 1) mapSize++
+            }
+        }
     }
     return res
 
-
-}
-const res = findLength([1, 2, 3, 2, 1], [3, 2, 1, 4, 7])
+};
+const res = minWindow('ADOBECODEBANC', 'ABC')
 console.log(res)
