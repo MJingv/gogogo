dc519393ee6257c9aefceca24993c23f96eac427
MJingv <625619954@qq.com>
2022-09-21 19:41:54 +0800

    +tree

--- tree2.js
+++ tree2.js
@@ -8,18 +8,22 @@
 // 第 543 题「 二叉树的直径」
 // 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
 
-var {BinarySearchTree} = require('./tree');
+const {BinarySearchTree, Node} = require('./tree');
 
 var diameterOfBinaryTree = function (root) {
 
     return root
-
 };
-const t = new BinarySearchTree()
-t.insert(5)
-t.insert(1)
-t.insert(9)
-t.insert(7)
-t.insert(17)
+const t = new Node(9)
+const t2 = new Node(1)
+const t3 = new Node(2)
+const t4 = new Node(3)
+const t5 = new Node(4)
+const t6 = new Node(5)
+t.right = t2
+t2.left = t3
+t2.right = t4
+t3.left = t5
+t3.right = t6
 const res = diameterOfBinaryTree(t)
 console.log(JSON.stringify(res))
