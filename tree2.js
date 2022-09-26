d31bf04586f5a0197bf28cdcb5b6eb811f0e3843
MJingv <625619954@qq.com>
2022-09-26 16:35:31 +0800

    buildTree

--- tree2.js
+++ tree2.js
@@ -175,4 +175,42 @@ var buildTree = function (preorder, inorder) {
 // console.log(JSON.stringify(res))
 
 
-// 力扣第 105 题「 从前序和中序遍历序列构造二叉树」
+// 889 题「 根据前序和后序遍历构造二叉树」
+// 输入：preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1] 输出：[1,2,3,4,5,6,7]
+var constructFromPrePost = function (preorder, postorder) {
+    const helper = (preorder, postorder, node) => {
+        if (!preorder.length || !postorder.length) return null
+        const val = preorder.shift()
+        const index = postorder.indexOf(val)
+        node = new Node(val)
+        const left = postorder.slice(0, index)
+        node.left = helper(preorder, left, node)
+        node.right = helper(preorder, left, node)
+        // console.log(val,left)
+
+        return node
+    }
+
+    return helper(preorder, postorder)
+};
+// const res = constructFromPrePost([1, 2, 4, 5, 3, 6, 7], [4, 5, 2, 6, 7, 3, 1])
+// console.log(JSON.stringify(res))
+// 力扣第 106 题「 从后序和中序遍历结果构造二叉树」
+// 输入：inorder = [9,3,15,20,7], postorder = [9,15,7,20,3] 输出：[3,9,20,null,null,15,7]
+var buildTree2 = function (inorder, postorder) {
+    const helper = (inorder, postorder, node) => {
+        if (!postorder.length || !inorder.length) return null
+        const val = postorder.pop()
+        const index = inorder.indexOf(val)
+        node = new Node(val)
+        const right = inorder.slice(index + 1)
+        const left = inorder.slice(0, index)
+        node.right = helper(right, postorder, node)
+        node.left = helper(left, postorder,node)
+        return node
+    }
+    return helper(inorder, postorder)
+};
+const res = buildTree2([9, 3, 15, 20, 7], [9, 15, 7, 20, 3])
+console.log(JSON.stringify(res))
+
