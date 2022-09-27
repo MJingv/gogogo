30e778dddefb3bba1ce706ef8dea0160f85a2cc5
MJingv <625619954@qq.com>
2022-09-27 20:00:29 +0800

    convertBST

--- bst.js
+++ bst.js
@@ -4,7 +4,45 @@ const {BinarySearchTree, Node} = require('./tree');
 // 输入：root = [3,1,4,null,2], k = 1 输出：1
 // 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
 var kthSmallest = function (root, k) {
-
+    const res = []
+    const traverse = (node, k) => {
+        if (!node) return null
+        traverse(node.left, k)
+        res.push(node.key)
+        traverse(node.right, k)
+    }
+    traverse(root, k)
+    return res[k - 1]
 };
-const res = kthSmallest()
+
+// const t = new Node(3)
+// const t2 = new Node(1)
+// const t3 = new Node(2)
+// const t4 = new Node(4)
+// t.left = t2
+// t2.right = t3
+// t.right = t4
+// const res = kthSmallest(t, 1)
+// console.log(res)
+
+// 力扣第 538/ 1038 题 把二叉搜索树转换为累加树
+var convertBST = function (root) {
+    let sum = 0
+    const traverse = (node) => {
+        if (!node) return null
+        traverse(node.right)
+        sum += node.key
+        node.key = sum
+        traverse(node.left)
+        return node
+    }
+    return traverse(root)
+};
+
+const t = new Node(1)
+const t2 = new Node(0)
+const t3 = new Node(2)
+t.left = t2
+t.right = t3
+const res = convertBST(t)
 console.log(res)
