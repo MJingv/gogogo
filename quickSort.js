472bd945846cdb0b805356481c274f7044b112e5
MJingv <625619954@qq.com>
2022-09-29 21:03:52 +0800

    sort

--- quickSort.js
+++ quickSort.js
@@ -19,7 +19,7 @@ const quickSort = (list, left, right) => {
     if (left >= right) return
     const p = partition(list, left, right)
     quickSort(list, left, p - 1)
-    quickSort(list, p + 1, right)
+    quickSort(list, p, right)
 }
 
 const sort = (list) => {
@@ -30,41 +30,7 @@ const sort = (list) => {
 }
 
 
-var sortArray = function (nums) {
-
-    const swap = (l, x, y) => [l[x], l[y]] = [l[y], l[x]]
-
-    const partition = (nums, left, right) => {
-        const mid = (left + right) >> 1
-        const val = nums[mid]
-        let i = left, j = right
-        while (i <= j) {
-            while (nums[i] < val) i++
-            while (nums[j] > val) j--
-            if (i <= j) {
-                swap(nums, i++, j--)
-            }
-        }
-        return i
-
-    }
-
-    const quickSort = (nums, left, right) => {
-        if (left >= right) return
-        const p = partition(nums, left, right)
-        quickSort(nums, left, p - 1)
-        quickSort(nums, p, right)
-    }
-
-
-    const len = nums.length
-    if (!len) return
-    quickSort(nums, 0, len - 1)
-    return nums
-};
-
-
-const res = sortArray([5, 1, 1, 2, 0, 0])
+const res = sort([5, 1, 1, 2, 0, 0])
 console.log(res)
 
 
