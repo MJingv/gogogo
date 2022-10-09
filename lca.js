d27e4dbfe456c9eacbb44d324fc93fa093aed94d
MJingv <625619954@qq.com>
2022-10-09 11:17:10 +0800

    lca4

--- lca.js
+++ lca.js
@@ -6,8 +6,10 @@ const {BinarySearchTree, Node} = require('./tree');
 
 
 var lowestCommonAncestor = function (root, p, q) {
+    // 如果一个节点能够在它的左右子树中分别找到p和q，则该节点为LCA节点。
     const traverse = (node, p, q) => {
         if (!node) return
+        // 一定存在p和q，所以如果等于p/q直接返回该节点
         if (node.key === p || node.key === q) return node
 
         const left = traverse(node.left, p, q)
@@ -35,16 +37,7 @@ t2.right = t5
 // console.log(res)
 
 
-// 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先
-var lowestCommonAncestor2 = function (root, p, q) {
-
-
-};
-// const res = lowestCommonAncestor2()
-// console.log(res)
-
-
-// 力扣第 1676 题「二叉树的最近公共祖先 IV」力扣第 236 题
+// 力扣第 1676 题「二叉树的最近公共祖先 IV」
 // 依然给你输入一棵不含重复值的二叉树，但这次不是给你输入p和q两个节点了，而是给你输入一个包含若干节点的列表nodes（这些节点都存在于二叉树中），让你算这些节点的最近公共祖先。
 var lowestCommonAncestor3 = function (root, nodes) {
     const map = new Map()
@@ -65,12 +58,39 @@ var lowestCommonAncestor3 = function (root, nodes) {
 
     return traverse(root, nodes)
 };
-const res = lowestCommonAncestor3(t, [t4, t5])
+const res = lowestCommonAncestor3(t, [t2, t3])
 console.log(res)
 
 // 力扣第 1644 题「二叉树的最近公共祖先 II」
-var lowestCommonAncestor4 = function (root, nodes) {
+// 给你输入一棵不含重复值的二叉树的，以及两个节点p和q，如果p或q不存在于树中，则返回空指针，否则的话返回p和q的最近公共祖先节点。
+var lowestCommonAncestor4 = function (root, p, q) {
+    let foundP = false, foundQ = false
+    const find = (root, pVal, qVal) => {
+        if (!root) return null
 
+        const left = find(root.left, pVal, qVal)
+        const right = find(root.right, pVal, qVal)
+        if (left && right) return root
+
+        if (root.val === pVal || root.val === qVal) {
+            if (root.val === pVal) foundP = true
+            if (root.val === qVal) foundQ = true
+            return root
+        }
+        return left ? left : right
+    }
+    const res = find(root, p.key, q.key)
+    if (!foundP || !foundP) return null
+    return res
 };
 // const res = lowestCommonAncestor4(t, [t6, t5])
+// console.log(res)
+
+
+// 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先
+var lowestCommonAncestor2 = function (root, p, q) {
+
+
+};
+// const res = lowestCommonAncestor2()
 // console.log(res)
