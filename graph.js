cb55853e2f2cb231437de8b55a662113d10ee921
MJingv <625619954@qq.com>
2022-10-17 19:36:28 +0800

    allPathsSourceTarget

--- graph.js
+++ graph.js
@@ -2,19 +2,22 @@
 // 输入：graph = [[1,2],[3],[3],[]] 输出：[[0,1,3],[0,2,3]] 解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
 var allPathsSourceTarget = function (graph) {
     const traverse = (graph, s = 0, path = []) => {
-        //节点s
         path.push(s)
         if (s === graph.length - 1) {
-            // 到头了
+            //到结尾节点
             res.push(path.slice())
             path.pop()
+            return
         }
+        for (let v of graph[s]) {
+            traverse(graph, v, path)
+        }
+        path.pop()
 
     }
     const res = []
     traverse(graph)
     return res
-
 };
 const res = allPathsSourceTarget([[1, 2], [3], [3], []])
 console.log(res)
