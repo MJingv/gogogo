b19ad8e104263fc618f475d82b49b0ad8fcbf568
MJingv <625619954@qq.com>
2022-11-08 21:09:08 +0800

    fib

--- dp2.js
+++ dp2.js
@@ -11,5 +11,12 @@ const res = coinChange([1, 2, 5], 11) ///3
 console.log(res)
 
 // 509. 斐波那契数
+var fib = function (n) {
+    if (n === 0 || n === 1) return n
+    if (n === 2) return 1
+
+    return fib(n - 1) + fib(n - 2)
+
+};
 
 // 剑指 Offer II 103. 最少的硬币数目
