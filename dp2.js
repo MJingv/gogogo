ba4149d8d1c4ed4dbea5de51120c8a434eadbd83
MJingv <625619954@qq.com>
2022-11-08 21:20:48 +0800

    fib2

--- dp2.js
+++ dp2.js
@@ -10,6 +10,23 @@ var fib = function (n) {
     return fib(n - 1) + fib(n - 2)
 
 };
+const fib1 = (n) => {
+    const dp = [0, 1,]
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
+}
+var fib2 = function (n) {
+    if (n === 0 || n === 1) return n
+    let [d1, d2] = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        const tmp = d1 + d2
+        d1 = d2
+        d2 = tmp
+    }
+    return d2
+};
 
 // 力扣第 322 题「 零钱兑换」
 var coinChange = function (coins, amount) {
