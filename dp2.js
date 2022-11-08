4b2cc26a9061167782ebe2c80d21208822bfcff8
MJingv <625619954@qq.com>
2022-11-08 21:09:52 +0800

    coinChange

--- dp2.js
+++ dp2.js
@@ -3,20 +3,24 @@
 // choice
 // dp
 
-// 力扣第 322 题「 零钱兑换」
-var coinChange = function (coins, amount) {
-
-};
-const res = coinChange([1, 2, 5], 11) ///3
-console.log(res)
-
 // 509. 斐波那契数
 var fib = function (n) {
     if (n === 0 || n === 1) return n
     if (n === 2) return 1
-
     return fib(n - 1) + fib(n - 2)
 
 };
 
+// 力扣第 322 题「 零钱兑换」
+var coinChange = function (coins, amount) {
+
+};
+
+
 // 剑指 Offer II 103. 最少的硬币数目
+var coinChange = function (coins, amount) {
+
+};
+
+const res = coinChange([1, 2, 5], 11) ///3
+console.log(res)
