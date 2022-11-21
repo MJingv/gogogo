752e502dd750107fc6b685ee15e9118350e6281c
MJingv <625619954@qq.com>
2022-11-21 23:04:09 +0800

    maxSubArray1

--- dp2.js
+++ dp2.js
@@ -144,5 +144,39 @@ var minDistance = function (word1, word2) {
 
 };
 
-const res = minDistance('horse', 'ros')
+// const res = minDistance('horse', 'ros')
+// console.log(res)
+
+// 53. 最大子数组和
+// 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+// 输入：nums = [-2,1,-3,4,-1,2,1,-5,4] 输出：6 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+var maxSubArray = function (nums) {
+    const len = nums.length
+    if (!len) return 0
+    if (len === 1) return nums[0]
+    const dp = Array(len).fill(0)//以i结尾的最大子串和
+    dp[0] = nums[0]
+    for (let i = 1; i < len; i++) {
+        dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
+
+    }
+    return Math.max(...dp)
+
+};
+var maxSubArray1 = function (nums) {
+    const len = nums.length
+    if (len === 0) return 0
+    if (len === 1) return nums[0]
+    let dp0 = nums[0], dp1 = 0, res = dp0
+    for (let i = 1; i < len; i++) {
+        dp1 = Math.max(dp0 + nums[i], nums[i])
+        dp0 = dp1
+        res = Math.max(res, dp0)
+    }
+    return res
+
+}
+
+const res = maxSubArray1([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 console.log(res)
+// 剑指 Offer 42. 连续子数组的最大和
