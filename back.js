95d705e75223b9ca016e9c907fa340f8fbf86874
MJingv <625619954@qq.com>
2022-11-23 20:54:28 +0800

    canPartition1

--- back.js
+++ back.js
@@ -3,14 +3,45 @@
 // 输入：nums = [1,5,11,5] 输出：true 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
 var canPartition = function (nums) {
     const len = nums.length
+    if (!len) return
+    let sum = nums.reduce((a, b) => a + b)
+    if (sum % 2) return false // sum奇数不能
+    sum = sum / 2
+    const dp = Array(len + 1).fill(0).map(i => Array(sum + 1).fill(0))
+    for (let i = 0; i <= len; i++) {
+        dp[i][0] = true
+    }
+    for (let i = 1; i <= len; i++) {
+        for (let j = 1; j <= sum; j++) {
+            if (j - nums[i - 1] < 0) {
+                dp[i][j] = dp[i - 1][j]
+            } else {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]
+            }
+        }
+    }
 
-
-
-
-
-
+    return dp[len][sum]
 };
-const res = canPartition([1, 5, 11, 5])
+const canPartition1 = (nums) => {
+    const len = nums.length
+    if (!len) return
+    let sum = nums.reduce((a, b) => a + b)
+    if (sum % 2) return false
+    sum = sum / 2
+    const dp = [true]
+    for (let i = 0; i < len; i++) {
+        for (let j = sum; j >= 0; j--) {
+            if (j >= nums[i]) {
+                dp[j] = dp[j] || dp[j - nums[i]]
+            }
+
+        }
+    }
+    return dp[sum] || false
+}
+
+const res = canPartition1([1, 5, 11, 5])
 console.log(res)
 
 // 剑指 Offer II 101. 分割等和子集
