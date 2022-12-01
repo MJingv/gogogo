dce8047a32222a4ec0e2be2b8bb43ccbe2b9af42
MJingv <625619954@qq.com>
2022-12-01 14:17:08 +0800

    +table

---
+++ makeTable/puppeteer.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setViewport({
+        width: 1000,
+        height: 5000,
+        deviceScaleFactor: 2
+    })
+    await page.goto('http://localhost:3000/');
+    //获取页面Dom对象
+
+    const captureScreenshotsOfElements = async (elements) => {
+        let i = 0;
+        for (const element of elements) {
+            await element.screenshot({ path: `img/${i}.jpeg`,quality:100 });
+            i += 1;
+        }
+    };
+
+    const gadgets = await page.$$('.ant-table-wrapper');
+    await captureScreenshotsOfElements(gadgets, page);
+
+
+    await browser.close();
+})();
