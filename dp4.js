5498d2eec42480270d417015414962a891638edd
MJingv <625619954@qq.com>
2022-12-03 14:05:00 +0800

    wordBreak

--- dp4.js
+++ dp4.js
@@ -24,28 +24,55 @@ var coinChange = function (coins, amount) {
 
 // 221. 最大正方形
 var maximalSquare = function (matrix) {
-    const len = matrix.length
-    if (!len) return 0
-    const dp = Array(len).fill(0).map(i => Array(len).fill(0))
 
-    for (let i = 0; i < len; i++) {
-        dp[i][0] = matrix[i][0] === '1' ? 1 : 0
+    const [m, n] = [matrix.length, matrix[0].length]
+    if (!m || !n) return 0
+    const dp = Array(m).fill(0).map(i => Array(n).fill(0))
+    let max = 0
+
+    for (let i = 0; i < m; i++) {
+        dp[i][0] = matrix[i][0]
+        if (dp[i][0] === '1') max = 1
     }
-    for (let j = 0; j < len; j++) {
-        dp[0][j] = matrix[0][j] === '1' ? 1 : 0
+    for (let j = 0; j < n; j++) {
+        dp[0][j] = matrix[0][j]
+        if (dp[0][j] === '1') max = 1
+
     }
 
-    for (let i = 1; i < len - 1; i++) {
-        for (let j = 1; j < len - 1; j++) {
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
             if (matrix[i][j] === '1') {
-                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1], dp[i][j + 1]) + 1 //求最小边长
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1 //求最小边长
+                max = Math.max(max, dp[i][j])
             }
         }
     }
 
-
-    return dp
+    return max * max
 
 };
-const res = maximalSquare([["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]])
+// const res = maximalSquare([["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]])
+// console.log(res)
+
+// 139. 单词拆分
+var wordBreak = function (s, wordDict) {
+    const len = s.length
+    const dp = Array(len + 1).fill(false)
+    dp[0] = true
+
+    for (let i = 1; i <= len; i++) {
+
+        for (let j = 0; j < i; j++) {
+            const str = s.slice(j, i)
+            if (dp[j] && wordDict.includes(str)) {
+                dp[i] = true
+            }
+        }
+    }
+    return dp[len]
+
+
+};
+const res = wordBreak(s = "applepenapple", wordDict = ["apple", "pen"])
 console.log(res)
