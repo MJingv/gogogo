b4fdce145abbbb43943cb76814fab0c39eedd7bc
MJingv <625619954@qq.com>
2022-12-26 21:04:38 +0800

    solveNQueens

--- dfs2.js
+++ dfs2.js
@@ -50,15 +50,53 @@ var permute1 = function (nums) {
     return res
 
 }
-const res = permute1([1, 2, 3])
-console.log(res)
+// const res = permute1([1, 2, 3])
+// console.log(res)
 
 // 力扣第 51 题「 N 皇后」
 var solveNQueens = function (n) {
 
+    const isValid = (board, row, col) => {
+        // 一行一行放Q 不用判断行
+        // 列存在重复Q
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === 'Q') return false
+        }
+        // 右上重复Q
+        for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] === 'Q') return false
+        }
+        // 左上重复Q
+        for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] === 'Q') return false
+        }
+        return true
+    }
+
+    const res = []
+
+    const helper = (board, row = 0) => {
+        console.log(row)
+        if (row === n - 1) {
+            return res.push(board.slice())
+        }
+
+        for (let col = 0; col < n; col++) {
+            if (!isValid(board, row, col)) continue
+
+            board[row][col] = 'Q'
+            helper(board, row + 1)
+            board[row][col] = '.'
+        }
+
+    }
+    const b = Array(n).fill(Array(n)).map(i => i.fill('.'))
+    helper(b, 0)
+    return res
+
 };
-// const res = solveNQueens(4)
-// console.log(res)
+const res = solveNQueens(4)
+console.log(res)
 
 // 784 字母大小写全排列
 // 输入: s = "3z4" 输出: ["3z4","3Z4"]
