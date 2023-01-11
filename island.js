92147ca694721c00d56447de6505e00c1e0dcef5
MJingv <625619954@qq.com>
2023-01-11 17:54:39 +0800

    closedIsland

--- island.js
+++ island.js
@@ -23,8 +23,46 @@ var numIslands = function (grid) {
             }
         }
     }
-
     return res
 };
-const res = numIslands([["1", "1", "0", "0", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "1", "0", "0"], ["0", "0", "0", "1", "1"]])
+// const res = numIslands([["1", "1", "0", "0", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "1", "0", "0"], ["0", "0", "0", "1", "1"]])
+// console.log(res)
+
+// 力扣第 1254 题「 统计封闭岛屿的数目」
+var closedIsland = function (grid) {
+    const [m, n] = [grid.length, grid[0].length]
+    let res = 0
+    const helper = (i, j) => {
+        if (i < 0 || i >= m || j < 0 || j >= n) return
+        if (grid[i][j] === 1) return; //已经是水了
+        grid[i][j] = 1
+        helper(i + 1, j)
+        helper(i - 1, j)
+        helper(i, j + 1)
+        helper(i, j - 1)
+    }
+
+    for (let i = 0; i < m; i++) {
+        helper(i, 0)
+        helper(i, n - 1)
+    }
+    for (let j = 0; j < n; j++) {
+        helper(0, j)
+        helper(m - 1, j)
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 0) {
+                res++
+                helper(i, j)
+            }
+
+        }
+    }
+    return res
+};
+const res = closedIsland([[0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 1, 1, 1, 0]])
 console.log(res)
+
+// 力扣第 1020 题「 飞地的数量」
