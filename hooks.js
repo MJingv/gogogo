d646f4e4e315941c13517ba3d8191ccd3d4f8bbc
MJingv <625619954@qq.com>
2023-01-28 16:02:09 +0800

    hooks+1

---
+++ hooks.js
@@ -0,0 +1,10 @@
+// react hooks
+// https://reactjs.org/docs/hooks-intro.html
+
+// Hooks allow you to reuse stateful logic without changing your component hierarchy.
+
+// why？
+// - Complex components become hard to understand
+    // Hooks let you split one component into smaller functions based on what pieces are related (such as setting up a subscription or fetching data)
+// - Classes confuse both people and machines
+    // Hooks let you use more of React’s features without classes
