5e119b8b4b81c0cee114aef9062bec854cc62975
MJingv <625619954@qq.com>
2023-01-29 19:12:49 +0800

    generateParenthesis

--- back.js
+++ back.js
@@ -105,16 +105,21 @@ var findTargetSumWays = function (nums, target) {
 // 1、一个「合法」括号组合的左括号数量一定等于右括号数量，这个很好理解。
 // 2、对于一个「合法」的括号字符串组合 p，必然对于任何 0 <= i < len(p) 都有：子串 p[0..i] 中左括号的数量都大于或等于右括号的数量。
 var generateParenthesis = function (n) {
+    const helper = (left, right, q = [], res) => {
+        if (left > right || left < 0 || right < 0) return
+        if (left === 0 && right === 0) return res.push(q.join(''))
 
-    // const helper = (left, right, track, res) => {
-    //     if () return
-    //     for () {
-    //         helper()
-    //     }
-    // }
+        q.push('(')
+        helper(left - 1, right, q, res)
+        q.pop()
 
-    let res = 0
-    helper(0, 0, '', res)
+        q.push(')')
+        helper(left, right - 1, q, res)
+        q.pop()
+    }
+
+    let res = []
+    helper(n, n, [], res)
     return res
 };
 const res = generateParenthesis(3)// ["((()))","(()())","(())()","()(())","()()()"]
