b38c75a3b34de868211e489a372d314d242ae2af
MJingv <625619954@qq.com>
2023-02-06 20:12:59 +0800

    2fen

---
+++ 2fen.js
@@ -0,0 +1,20 @@
+// 力扣第 392 题「 判断子序列」
+// 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+// 输入：s = "abc", t = "ahbgdc" 输出：true
+var isSubsequence = function (s, t) {
+    const [len1, len2] = [s.length, t.length]
+    let [a, b] = [0, 0]
+    while (a < len1 && b < len2) {
+        const char = s[a]
+        if (t[b] === char) {
+            a++
+            b++
+        } else {
+            b++
+        }
+    }
+    return a === len1
+
+};
+// const res = isSubsequence('abc', 'ahbgdc')
+// console.log(res)
