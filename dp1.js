88dddc0d7edbf99cc142272bd87d85f05d7f6326
MJingv <625619954@qq.com>
2023-03-10 19:41:54 +0800

    hasPathSum

--- dp1.js
+++ dp1.js
@@ -2,6 +2,21 @@
 // bad case 状态 选择 定义dp
 // 最优子结构，问题相互独立
 
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+const t1 = new TreeNode(1)
+const t0 = new TreeNode(0)
+t1.left = t0
+// t0.left = new TreeNode(0)
+const t3 = new TreeNode(3)
+t3.left = new TreeNode(2)
+t3.right = new TreeNode(4)
+t1.right = t3
+
 
 // 509. 斐波那契数
 // 剑指 Offer 10- I. 斐波那契数列
@@ -82,9 +97,8 @@ var climbStairs = function (n) {
     }
     return b
 };
-const res = climbStairs(3)
-console.log(res)
-
+// const res = climbStairs(3)
+// console.log(res)
 
 // 面试题 三步问题
 // 三步问题。有个小孩正在上楼梯，楼梯有n阶台阶，小孩一次可以上1阶、2阶或3阶。实现一种方法，计算小孩有多少种上楼梯的方式。结果可能很大，你需要对结果模1000000007。
@@ -105,7 +119,45 @@ var waysToStep = function (n) {
 // console.log(res)
 
 // 剑指 Offer 14- I. 剪绳子
+// 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+// 输入: 10 输出: 36 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+var cuttingRope = function (n) {
+    if (n === 2) return 1
+    const dp = Array(n + 1).fill(-Infinity)
+    dp[1] = 1
+    dp[2] = 1
+    for (let i = 3; i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (j < i) {
+                // 剪成i和j就不剪了
+                dp[i] = Math.max(dp[i], dp[i - 1], (i - j) * j, dp[i - j] * j)
+            }
+
+        }
+    }
+    return dp[n]
+
+};
+// const res = cuttingRope(10)
+// console.log(res)
+
 // 112. 路径总和
+// 判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。如果存在，返回 true ；否则，返回 false 。
+var hasPathSum = function (root, targetSum) {
+    if (!root) return false
+    let res = false
+    const helper = (node, sum) => {
+        if (!node) return
+        if (!node.left && !node.right && sum === targetSum) return res = true
+        node.left && helper(node.left, sum + node.left.val)
+        node.right && helper(node.right, sum + node.right.val)
+        return false
+    }
+    helper(root, root.val)
+    return res
+};
+const res = hasPathSum(t1, 8)
+console.log(res)
 
 // 115. 不同的子序列
 
