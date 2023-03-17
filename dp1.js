0d0f67af6d58896fdf9e708245a18d3081ba976a
MJingv <625619954@qq.com>
2023-03-17 16:07:44 +0800

    getMaximumGenerated

--- dp1.js
+++ dp1.js
@@ -513,6 +513,27 @@ var countBits = function (n) {
 
     return dp
 };
-const res = countBits(5)
-console.log(res)
+// const res = countBits(5)
+// console.log(res)
 
+// 1646 获取生成数组中的最大值
+// 给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
+// nums[0] = 0
+// nums[1] = 1
+// 当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
+// 当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
+// 返回生成数组 nums 中的 最大 值。
+var getMaximumGenerated = function (n) {
+    if (n === 0) return 0
+    if (n === 1) return 1
+    const dp = Array(n + 1).fill(0)
+    dp[1] = 1
+    for (let i = 0; i <= n; i++) {
+        let val = 2 * i
+        if (val >= 2 && val <= n) dp[val] = dp[i]
+        if (val + 1 >= 2 && val + 1 <= n) dp[val + 1] = dp[i] + dp[i + 1]
+    }
+    return Math.max(...dp)
+};
+const res = getMaximumGenerated(7) //3
+console.log(res)
