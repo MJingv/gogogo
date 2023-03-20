c57595b043d6a15cc9c895e99fcaacfd4cfd00db
MJingv <625619954@qq.com>
2023-03-20 17:22:40 +0800

    maxProfit309

--- profit.js
+++ profit.js
@@ -77,16 +77,16 @@ var maxProfit309 = function (prices) {
             continue
         }
         if (i === 1) {
-            dp[1][0] = Math.max(0, dp[0][1] + prices[i])
-            dp[1][1] = -prices[1]
+            dp[1][0] = Math.max(dp[0][0], dp[0][1] + prices[1])
+            dp[1][1] = Math.max(dp[i - 1][1], -prices[1])
             continue
 
         }
-        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][1] + prices[i])
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
         dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i])
     }
 
-    return dp
+    return dp[len - 1][0]
 };
 const res = maxProfit309([1, 2, 3, 0, 2])
 console.log(res)
