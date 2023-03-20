b6ce331671c7d7c5df8d39311d44fd4291c832cf
MJingv <625619954@qq.com>
2023-03-20 16:24:09 +0800

    maxProfit+1

--- profit.js
+++ profit.js
@@ -5,13 +5,21 @@
 // 输入：prices = [7,1,5,3,6,4] 输出：7 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。 总利润为 4 + 3 = 7 。
 var maxProfit = function (prices) {
     const n = prices.length
-    const dp = Array(n).fill(0)
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = i + 1; j < n; j++) {
-            dp[i] = Math.max(dp[i], prices[j] - prices[i])
-        }
+    const dp = Array(n).fill(0).map(i => Array(2).fill(0))
+    // 第i天卖了
+    // dp[i][0] =Math.max(dp[i-1][0],dp[i-1][1]+prices[i])
+    // 第i天买了
+    // dp[i][1] =Math.max(dp[i-1][1],-prices[i])
+    dp[0][0] = 0
+    dp[0][1] = -prices[0]
+    for (let i = 1; i < n; i++) {
+        // 现在没有股票
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+        // 现在有股票
+        dp[i][1] = Math.max(dp[i - 1][1], -prices[i])
+
     }
-    return dp
+    return dp[n - 1][0]
 };
 const res = maxProfit([7, 6, 4, 3, 1])
 console.log(res)
