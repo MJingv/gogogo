0ee5e8847a38f92d18750542a8ac559b09044d57
MJingv <625619954@qq.com>
2023-03-23 16:51:01 +0800

    lengthOfLIS

--- dp5.js
+++ dp5.js
@@ -2,13 +2,18 @@
 // 输入：nums = [10,9,2,5,3,7,101,18] 输出：4 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
 var lengthOfLIS = function (nums) {
     const len = nums.length
-    const dp = Array(len).fill(0)
-    dp[0] = 1
-    let res = 0
-    for (let i = 1; i < len; i++) {
-        res = Math.max(dp[i], res)
+    // 以i结尾的最大长度
+    let res = 1
+    const dp = Array(len).fill(1)
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1)
+                res = Math.max(res, dp[i])
+            }
+        }
     }
-    return dp
+    return res
 };
-const res = lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])
+const res = lengthOfLIS([4, 10, 4, 3, 8, 9])
 console.log(res)
