2003bd599dfacfa645ec224b24a799d9d81f9de5
MJingv <625619954@qq.com>
2023-03-24 11:35:14 +0800

    minCost

--- dp5.js
+++ dp5.js
@@ -57,7 +57,20 @@ var largestDivisibleSubset = function (nums) {
 // const res = largestDivisibleSubset([1, 2, 4, 8, 16])
 // console.log(res)
 
+
 // 剑指 Offer II 091. 粉刷房子
+// 假如有一排房子，共 n 个，每个房子可以被粉刷成红色、蓝色或者绿色这三种颜色中的一种，你需要粉刷所有的房子并且使其相邻的两个房子颜色不能相同。
+// 输入: costs = [[17,2,17],[16,16,5],[14,3,19]] 输出: 10 解释: 将 0 号房子粉刷成蓝色，1 号房子粉刷成绿色，2 号房子粉刷成蓝色。 最少花费: 2 + 5 + 3 = 10。
 var minCost = function (costs) {
 
+    const len = costs.length
+    if (!len) return 0
+    for (let i = 1; i < len; i++) {
+        costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2])
+        costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2])
+        costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1])
+    }
+    return Math.min(...costs[len - 1])
 };
+const res = minCost([[17, 2, 17], [16, 16, 5], [14, 3, 19]])
+console.log(res)
