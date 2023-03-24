aa1443618625b3087aa3c565f3ef6346c1e53b1c
MJingv <625619954@qq.com>
2023-03-24 16:25:14 +0800

    subarraySum

--- dp5.js
+++ dp5.js
@@ -76,26 +76,20 @@ var minCost = function (costs) {
 // console.log(res)
 
 // 剑指 Offer II 010 和为k的子数组
+// 有负数啊，前缀和解
 // 给定一个整数数组和一个整数 k ，请找到该数组中和为 k 的连续子数组的个数。
 // 输入:nums = [1,1,1], k = 2 输出: 2 解释: 此题 [1,1] 与 [1,1] 为两种不同的情况
 var subarraySum = function (nums, k) {
     const len = nums.length
-
-    // 以nums[i]结尾的和为k的子数字个数
-    const dp = Array(len).fill(0)
+    const map = new Map()
+    map.set(0, 1)
+    let [res, sum] = [0, 0]
     for (let i = 0; i < len; i++) {
-        if (nums[i] > k) continue
-        if (nums[i] === k) {
-            dp[i] = 1
-        }
-        let sum = 0
-        for (let j = 0; j < i; j++) {
-            if (sum === k) dp[i] += 1
-
-
-        }
+        sum += nums[i]
+        res += map.get(sum - k) || 0
+        map.set(sum,(map.get(sum)||0)+1)
     }
-    return dp
+    return res
 };
-const res = subarraySum([1, 2, 3], 3)
+const res = subarraySum([1, -1, 0], 0)
 console.log(res)
