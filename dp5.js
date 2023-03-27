e2d3a32a98e9e4a466b779e7fd1c76d09a1646af
MJingv <625619954@qq.com>
2023-03-27 11:17:21 +0800

    wordBreak

--- dp5.js
+++ dp5.js
@@ -95,5 +95,40 @@ var maxDistance = function (colors) {
     }
     return max
 };
-const res = maxDistance([1, 8, 3, 8, 3])
+// const res = maxDistance([1, 8, 3, 8, 3])
+// console.log(res)
+
+
+// 115. 不同的子序列 hard
+
+// 139. 单词拆分
+// 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+// 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+// 输出: true
+// 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+var wordBreak = function (s, wordDict) {
+    const n = s.length
+    const dp = Array(n + 1).fill(false)
+    dp[0] = true
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < i; j++) {
+            const str = s.slice(j, i)
+            if (dp[j] && wordDict.includes((str))) dp[i] = true
+        }
+
+    }
+    return dp[n]
+};
+const res = wordBreak('applepenapple', ["apple", "pen"])
 console.log(res)
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+// 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"] 输出: false
+
+
+// 221. 最大正方形
+// 343. 整数拆分
+// 576. 出界的路径数
+// 63. 不同路径 II
+// 91. 解码方法
+// 剑指 Offer II 097. 子序列的数目
