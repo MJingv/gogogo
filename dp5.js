6d93964853a5b130a5e7cc6004c25ee072bb388f
MJingv <625619954@qq.com>
2023-03-30 15:24:25 +0800

    massage

--- dp5.js
+++ dp5.js
@@ -303,6 +303,22 @@ var tribonacci1 = function (n) {
     }
     return c
 }
+// const res = tribonacci1(4)
+// console.log(res)
 
-const res = tribonacci1(4)
+// 17.16 按摩师
+// 一个有名的按摩师会收到源源不断的预约请求，每个预约都可以选择接或不接。在每次预约服务之间要有休息时间，因此她不能接受相邻的预约。给定一个预约请求序列，替按摩师找到最优的预约集合（总预约时间最长），返回总的分钟数。
+// 输入： [2,7,9,3,1] 输出： 12 解释： 选择 1 号预约、 3 号预约和 5 号预约，总时长 = 2 + 9 + 1 = 12。
+var massage = function (nums) {
+    const len = nums.length
+    if (!len) return 0
+    const dp = Array(len).fill(Infinity)
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0], nums[1])
+    for (let i = 2; i < len; i++) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+    }
+    return dp[len - 1]
+};
+const res = massage([2, 7, 9, 3, 1])
 console.log(res)
