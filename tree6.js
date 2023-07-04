29ac6575046a35d7e496d1e353588f0cf2a6f2b4
MJingv <625619954@qq.com>
2023-07-04 19:30:54 +0800

    kthSmallest

--- tree6.js
+++ tree6.js
@@ -444,13 +444,15 @@ var findDuplicateSubtrees = function (root) {
 
 // 第 230 题「二叉搜索树中第 K 小的元素」
 var kthSmallest = function (root, k) {
+    let res = Infinity
     const helper = (node) => {
         if (!node) return null
         helper(node.left)
-        if (!k) return node.val
+        if (k-- === 1) return res = node.val
         helper(node.right)
     }
-    return helper(root)
+    helper(root)
+    return res
 };
-const res = kthSmallest(t1, 1)
-console.log(res)
+// const res = kthSmallest(t1, 1)
+// console.log(res)
