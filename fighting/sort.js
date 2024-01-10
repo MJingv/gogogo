d70bca8ec74d8315f56e46eefecec8fc58e6c492
MJingv <625619954@qq.com>
2024-01-10 19:21:02 +0800

    +quicksort

--- fighting/sort.js
+++ fighting/sort.js
@@ -1,26 +1,20 @@
 const quickSort = (arr) => {
-    if (arr.length <= 1) {
-        return arr;
-    }
-
-    const pivot = arr[Math.floor(arr.length / 2)];
-    const left = [];
-    const right = [];
-    const equal = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else if (arr[i] > pivot) {
-            right.push(arr[i]);
+    const len = arr.length
+    if (len <= 1) return arr
+    const left = [], right = [], equal = []
+    const pivot = arr[Math.floor(len / 2)]
+    for (let i = 0; i < len; i++) {
+        if (arr[i] > pivot) {
+            right.push(arr[i])
+        } else if (arr[i] < pivot) {
+            left.push(arr[i])
         } else {
-            equal.push(arr[i]);
+            equal.push(arr[i])
         }
     }
-
-    return [...quickSort(left), ...equal, ...quickSort(right)];
+    return [...quickSort(left), ...equal, ...quickSort(right)]
 };
 
-const list = [3, 0, 5, 2, 4, 1, 1, 4, 99];
-const sortedList = quickSort(list);
-console.log(sortedList);
+const arr = [3, 0, 5, 2, 4, 1, 1, 4, 99];
+const sortedArr = quickSort(arr);
+console.log(sortedArr); // 输出：[0, 1, 1, 2, 3, 4, 4, 5, 99]
