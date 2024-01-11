4aa06034d91e97d0d4dd23e5570bfe1f8e6df9c7
MJingv <625619954@qq.com>
2024-01-11 20:11:11 +0800

    binarySearch1

--- fighting/array.js
+++ fighting/array.js
@@ -10,5 +10,22 @@ const binarySearch = (arr = [], target, i = 0, j = arr.length - 1) => {
         return binarySearch(arr, target, mid + 1, j)
     }
 }
-const res = binarySearch([1, 3, 8, 9, 14, 99], 14)
+const binarySearch1 = (arr = [], target) => {
+    const len = arr.length
+    let i = 0, j = len - 1
+
+    while (i <= j) {
+        const mid = Math.floor((i + j) / 2)
+        if (arr[mid] === target) {
+            return mid
+        } else if (arr[mid] > target) {
+            j = mid - 1
+        } else if (arr[mid] < target) {
+            i = mid + 1
+        }
+    }
+    return -1
+
+}
+const res = binarySearch1([1, 3, 8, 9, 14, 99], 14)
 console.log(res)
