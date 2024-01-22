aae62ef3c832ca3f43e1af5622736bf08a346af0
MJingv <625619954@qq.com>
2024-01-22 19:39:15 +0800

    mergesort

--- fighting/tree.js
+++ fighting/tree.js
@@ -166,5 +166,52 @@ const connect = (root) => {
 }
 
 
-const res = connect(t1)
+// const res = connect(t1)
+
+
+const quickSort = (list) => {
+    const len = list.length
+    if (!len) return []
+    const left = [], right = [], equal = []
+    const pivot = Math.floor(len / 2)
+    for (let i = 0; i < len; i++) {
+        const target = list[pivot]
+        const cur = list[i]
+        if (cur === target) equal.push(cur)
+        if (cur > target) right.push(cur)
+        if (cur < target) left.push(cur)
+    }
+
+    return [...quickSort(left), ...equal, ...quickSort(right)]
+}
+
+
+const merge = (a = [], b = []) => {
+    const res = []
+    const len1 = a.length
+    const len2 = b.length
+    let i = 0, j = 0
+    while (i < len1 && j < len2) {
+        if (a[i] < b[j]) {
+            res.push(a[i++])
+        } else {
+            res.push(b[j++])
+        }
+    }
+    while (i < len1) res.push(a[i++])
+    while (j < len2) res.push(b[j++])
+    return res
+}
+
+const mergeSort = (list = []) => {
+    const len = list.length
+    if (len <= 1) return list
+    const mid = Math.floor(len / 2)
+    const left = list.slice(0, mid)
+    const right = list.slice(mid)
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+
+const res = mergeSort([9, 1, 5, 2, -2, 100, 0, 99, 100])
 console.log(res)
