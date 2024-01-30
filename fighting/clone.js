25e20981d5ebd7c2dd1febf4f923a5d6bef3afae
MJingv <625619954@qq.com>
2024-01-30 21:30:27 +0800

    deepClone

--- fighting/clone.js
+++ fighting/clone.js
@@ -2,13 +2,14 @@
 // instanceof主要检测构造函数的prototype是否在原型连上 aka 一个实例是否是某个类型
 
 const deepClone = (obj, hash = new WeakMap()) => {
-    if (obj === null) return obj
+    if (!obj) return obj
     if (typeof obj !== 'object') return obj
 
     if (obj instanceof Date) return new Date(obj)
     if (obj instanceof RegExp) return new RegExp(obj)
 
     if (hash.has(obj)) return hash.get(obj)
+
     let cloneObj = new obj.constructor
     hash.set(obj, cloneObj)
 
