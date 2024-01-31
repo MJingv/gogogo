2415d11b77d499ced050ad69485dfb9513986781
MJingv <625619954@qq.com>
2024-01-31 16:54:21 +0800

    +throttle

---
+++ fighting/closure.js
@@ -0,0 +1,40 @@
+// 闭包不可替代的case
+// 1.私有变量
+// 2.节流防抖
+// 3.异步
+
+const createCounter = () => {
+    // 1.私有变量
+    let counter = 0
+    return () => counter++
+}
+const counter = createCounter()
+// console.log(counter())
+// console.log(counter())
+// console.log(counter())
+
+
+const throttle = (fn, delay) => {
+    // 间隔1000ms执行
+    let last = 0
+    return (...args) => {
+        let now = new Date()
+        if (now - last > delay) {
+            fn.apply(this, args)
+            last = now
+        }
+    }
+}
+const test = throttle(() => console.log(999), 5000)
+window.addEventListener('scroll', test)
+window.removeEventListener('scroll', test)
+setTimeout(() => {
+    throttle(() => console.log(1111), 500)
+}, 2000)
+
+
+const debounce = () => {
+    // 防抖 强制停止1s后再执行。eg.输入300ms后才能再输入
+}
+
+
