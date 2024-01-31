3b1d4edbeb0bcd545acf272220ca7c3388518ea9
MJingv <625619954@qq.com>
2024-01-31 17:19:35 +0800

    +promise

--- fighting/closure.js
+++ fighting/closure.js
@@ -25,12 +25,12 @@ const throttle = (fn, delay) => {
         }
     }
 }
-const test = throttle(() => console.log(999), 5000)
-window.addEventListener('scroll', test)
-window.removeEventListener('scroll', test)
-setTimeout(() => {
-    throttle(() => console.log(1111), 500)
-}, 2000)
+// const test = throttle(() => console.log(999), 5000)
+// window.addEventListener('scroll', test)
+// window.removeEventListener('scroll', test)
+// setTimeout(() => {
+//     throttle(() => console.log(1111), 500)
+// }, 2000)
 
 
 const debounce = (fn, delay) => {
@@ -44,6 +44,39 @@ const debounce = (fn, delay) => {
 }
 
 
+// 异步编程
+
+// setTimeout
+const async1 = (name) => {
+    setTimeout(() => {
+        // console.log(111, name)
+    }, 2000)
+}
+// promise
+let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(2222222)
+    }, 1000)
+})
+// p.then((v) => console.log(v, 444))
+
+// async
+const a = () => new Promise((res, rej) => {
+    setTimeout(() => {
+        console.log(777777777)
+        res('res')
+    }, 2000)
+})
+const t = async () => {
+    try {
+        const res = await a()
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+
+    }
+}
+t().then((val) => console.log(val))
 
 
 
