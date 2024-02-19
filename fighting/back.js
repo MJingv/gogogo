ddb81519285c15aad976655d9bf14511af452c17
MJingv <625619954@qq.com>
2024-02-19 20:09:25 +0800

    combine

--- fighting/back.js
+++ fighting/back.js
@@ -1,4 +1,5 @@
-// 回溯专题
+// 回溯专题 -排列组合
+
 // 回溯算法是在遍历「树枝」，DFS 算法是在遍历「节点」
 
 // result = []
@@ -14,6 +15,8 @@
 
 
 // 46. 全排列
+// 剑指 Offer II 083. 没有重复元素集合的全排列
+
 // 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
 var permute = function (nums) {
     const len = nums.length
@@ -71,11 +74,91 @@ var permuteUnique = function (nums) {
     return res
 
 };
-const res = permuteUnique([1, 1, 2])
+// const res = permuteUnique([1, 1, 2])
+
+// 力扣第 78 题「子集」
+// 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+// 输入：nums = [1,2,3] 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+var subsets = function (nums) {
+    const len = nums.length
+    const [res, path] = [[], []]
+    const helper = (start = 0) => {
+        res.push(path.slice())
+        for (let i = start; i < len; i++) {
+            path.push(nums[i])
+            helper(i + 1)
+            path.pop()
+        }
+    }
+    helper()
+    return res
+};
+// const res = subsets([1, 2, 3])
 
 
-// 剑指 Offer II 083. 没有重复元素集合的全排列
+// 比如力扣第 77 题「组合」：
+// 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+var combine = function (n, k) {
+    if (!k || !n) return []
+    const [res, path] = [[], []]
+    const helper = (start = 1) => {
+        if (path.length === k) return res.push(path.slice())
+        for (let i = start; i <= n; i++) {
+            path.push(i)
+            helper(i + 1)
+            path.pop()
+        }
+
+    }
+    helper()
+    return res
+
+};
+const res = combine(4, 2)//[[2, 4], [3, 4], [2, 3], [1, 2], [1, 3], [1, 4],]
+
+
 // 51. N 皇后
+// 输入：n = 4 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+var solveNQueens = function (n) {
+    const isValid = (board, row, col) => {
+        // 一行一行放Q 不用判断行
+        // 列存在重复Q
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === 'Q') return false
+        }
+        // 右上重复Q
+        for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] === 'Q') return false
+        }
+        // 左上重复Q
+        for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] === 'Q') return false
+        }
+        return true
+    }
+
+    const res = []
+
+    const helper = (board, row = 0) => {
+        if (row === board.length) {
+            return res.push(board.map((row) => row.join("")));
+        }
+
+        for (let col = 0; col < n; col++) {
+            if (!isValid(board, row, col)) continue
+
+            board[row][col] = 'Q'
+            helper(board, row + 1)
+            board[row][col] = '.'
+        }
+
+    }
+    const b = Array(n).fill(0).map(i => Array(n).fill('.'))
+    helper(b, 0)
+    return res
+
+};
+// const res = solveNQueens(4)
 // 52. N皇后 II
 
 
