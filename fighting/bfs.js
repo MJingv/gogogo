3c53c89fae39a672812a2cbb9ee6723093ca0dcc
MJingv <625619954@qq.com>
2024-02-20 17:10:52 +0800

    updateMatrix

--- fighting/bfs.js
+++ fighting/bfs.js
@@ -202,10 +202,37 @@ var largestValues = function (root) {
 
 
 };
-const res = largestValues(t1)
+// const res = largestValues(t1)
 
 // LeetCode 542. 01 矩阵
+// 给定一个由 0 和 1 组成的矩阵 mat ，请输出一个大小相同的矩阵，其中每一个格子是 mat 中对应位置元素到最近的 0 的距离。
+var updateMatrix = function (mat) {
+    const [m, n] = [mat.length, mat[0].length]
+    const dp = Array(m).fill(0).map(() => Array(n).fill(Infinity))
+    // 左+上
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 0) {
+                dp[i][j] = 0
+            } else {
+                if (i > 0) dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1)
+                if (j > 0) dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1)
+            }
+        }
+    }
+    // 右+下
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            if (i < m - 1) dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1)
+            if (j < n - 1) dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1)
+        }
+    }
 
+
+    return dp
+};
+const res = updateMatrix([[0, 0, 0], [0, 1, 0], [1, 1, 1]])
+// 输入：mat = [[0,0,0],[0,1,0],[1,1,1]] 输出：[[0,0,0],[0,1,0],[1,2,1]]
 
 // LeetCode 994. 腐烂的橘子
 
