2750ce5373cb3fe5eae1a87636865b3b0b4360ec
MJingv <625619954@qq.com>
2024-02-25 15:47:42 +0800

    searchMatrix

--- 150/1.js
+++ 150/1.js
@@ -320,5 +320,58 @@ var isValidBST = function (root, min = -Infinity, max = Infinity) {
     return left && right
 
 };
-const res = isValidBST(t1)
+// const res = isValidBST(t1)
+
+// ------------------------------------------------------------------------------------二分------------------------------------------------------------------------------------
+var searchInsert = function (nums, target) {
+    const len = nums.length
+    let i = 0, j = len - 1
+    while (i <= j) {
+        const mid = Math.floor((i + j) / 2)
+        if (target === nums[mid]) {
+            return mid
+        } else if (target < nums[mid]) {
+            j = mid - 1
+
+        } else if (target > nums[mid]) {
+            i = mid + 1
+
+        }
+    }
+    return j + 1
+
+};
+// const res = searchInsert([1, 3, 5, 6], 2)
+
+
+// 74 搜索二维矩阵
+// matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+var searchMatrix = function (matrix, target) {
+    const [m, n] = [matrix.length, matrix[0].length]
+    let i = 0, j = m * n - 1
+    while (i <= j) {
+        const mid = Math.floor((i + j) / 2)
+        const cur = matrix[Math.floor(mid / n)][mid % n]
+        if (cur === target) {
+            return true
+        } else if (cur < target) {
+            i = mid + 1
+
+        } else if (cur > target) {
+            j = mid - 1
+        }
+    }
+    return false
+
+};
+const res = searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 5)
+
+
+//162
+// 33
+// 34
+// 153
+// 4
+
+
 console.log(res)
