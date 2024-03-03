62ddcb1e11b62db63e861e3c4fbf122567845c7d
MJingv <625619954@qq.com>
2024-03-03 22:00:57 +0800

    combinationSum3

--- 150/maybe.js
+++ 150/maybe.js
@@ -1,3 +1,18 @@
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = val
+        this.left = left || null
+        this.right = right || null
+    }
+}
+
+const t0 = new TreeNode(0)
+const t2 = new TreeNode(2)
+const t4 = new TreeNode(4)
+const t3 = new TreeNode(3, t2, t4)
+const t1 = new TreeNode(1, t0, t3)
+
+
 // string转int
 const f1 = (s) => {
     const num = Number(s)
@@ -23,14 +38,6 @@ const f2 = (nums = []) => {
 
 
 // n个节点的二叉树
-
-class TreeNode {
-    constructor(val, left, right) {
-        this.val = val
-        this.left = left || null
-        this.right = right || null
-    }
-}
 
 
 var generateTrees = function (n) {
@@ -63,8 +70,77 @@ var generateTrees = function (n) {
     return helper(1, n)
 
 };
-const res = JSON.stringify(generateTrees(4))
+// const res = JSON.stringify(generateTrees(4))
 
 // 抢红包
 
+
+// 124 二叉树中最大的路径和
+var maxPathSum = function (root) {
+    if (!root) return 0
+    let res = -Infinity
+    const helper = (node) => {
+        if (!node) return 0
+        const left = Math.max(0, helper(node.left))
+        const right = Math.max(0, helper(node.right))
+        const cur = node.val + left + right
+        res = Math.max(res, cur)
+        return node.val + Math.max(left, right)
+    }
+    helper(root)
+    return res
+};
+// const res = maxPathSum(t1)
+
+// 20
+var isValid = function (s) {
+    // 只入栈左边的
+    const stack = []
+    for (let val of s) {
+        if (val === '(') {
+            stack.push(')')
+        } else if (val === '[') {
+            stack.push(']')
+        } else if (val === '{') {
+            stack.push('}')
+        } else {
+            if (stack.pop() !== val) return false
+        }
+    }
+    return stack.length === 0
+
+};
+// const res = isValid('(){[]{}')
+
+
+// 216 组合总和
+// 输入: k = 3, n = 9 输出: [[1,2,6], [1,3,5], [2,3,4]] 解释: 1 + 2 + 6 = 9 1 + 3 + 5 = 9 2 + 3 + 4 = 9 没有其他符合的组合了。
+var combinationSum3 = function (k, n) {
+    if (!k || !n) return []
+    const res = []
+    let path = []
+    const helper = (i = 1, sum = 0) => {
+        if (sum === n && path.length === k) {
+            res.push(path.slice())
+        }
+        if (path.length === k || i === 10) return
+        path.push(i)
+        helper(i + 1, sum + i)
+        path.pop()
+        helper(i + 1, sum)
+    }
+    helper()
+    return res
+
+};
+const res = combinationSum3(3, 9)
+// 206
+// 3
+// 236
+// 450
+// 146
+// 53
+// 207
+// 33
+// 56
 console.log(res)
