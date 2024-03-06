9cfd2b43361067442b1fe2239cf69e5168029cdf
MJingv <625619954@qq.com>
2024-03-06 15:28:54 +0800

    isInterleave

--- 150/1.js
+++ 150/1.js
@@ -286,7 +286,7 @@ var rangeSumBST = function (root, low, high) {
     return res
 
 };
-const res = rangeSumBST(t1, 1, 3)
+// const res = rangeSumBST(t1, 1, 3)
 
 // ------------------------------------------------------------------------------------二叉搜索树------------------------------------------------------------------------------------
 
@@ -544,27 +544,292 @@ var mySqrt = function (x) {
 // const res = mySqrt(10)
 
 
-// 50 pow（x,n）
+// 50 pow（x,n）求幂
+var myPow = function (x, n) {
+    if (n === 0) return 1
+    if (n < 0) {
+        x = 1 / x
+        n = -n
+    }
+    let res = 1
+    while (n > 0) {
+        if (n % 2 === 1) {
+            res = res * x
+        }
+        x *= x
+        n = Math.floor(n / 2)
+    }
+    return res
+
+
+};
+// const res = myPow(2.00000, 10)
 // 149 直线上最多的点数 hard
 
 
 // ------------------------------------------------------------------------------------dp一维------------------------------------------------------------------------------------
 // 70 爬楼梯
+var climbStairs = function (n) {
+    const dp = Array(n + 1).fill(0)
+    dp[1] = 1
+    dp[2] = 2
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
+};
+// const res = climbStairs(3)
+
 // 198 打家劫舍
+var rob = function (nums) {
+    const len = nums.length
+    if (len === 0) return 0
+    if (len === 1) return nums[0]
+    const dp = Array(len).fill(0)
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0], nums[1])
+    for (let i = 2; i < len; i++) {
+        dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1])
+    }
+    return Math.max(...dp)
+};
+// const res = rob([0])
+
+
 // 139 单词拆分
+var wordBreak = function (s, wordDict) {
+    const n = s.length, len = wordDict.length
+    const dp = Array(n + 1).fill(false)
+    dp[0] = true
+
+    for (let i = 1; i <= n; i++) {//i结尾
+        for (let j = 0; j < i; j++) {//j开头
+            const tmp = s.slice(j, i)
+            if (wordDict.includes(tmp) && dp[j]) dp[i] = true
+
+        }
+
+    }
+
+    return dp[n]
+};
+// const res = wordBreak("leetcode", ["leet", "code"])
+
 // 322 零钱兑换
+// 计算并返回可以凑成总金额所需的 最少的硬币个数
+// 输入：coins =  amount = 11 输出：3 解释：11 = 5 + 5 + 1
+
+var coinChange = function (coins, amount) {
+    const len = coins.length
+    if (!amount) return 0
+    const dp = Array(amount + 1).fill(Infinity)
+    dp[0] = 0
+
+    for (let i = 1; i <= amount; i++) {
+        for (let j = 0; j < len; j++) {
+            const cur = coins[j]
+            if (i >= cur) {
+                dp[i] = Math.min(dp[i], dp[i - cur] + 1)
+            }
+        }
+
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount]
+
+
+};
+// const res = coinChange([1, 2, 5], 11)
+
 // 300 最长递增子序列
+// 严格递增
+// 输入：nums = [10,9,2,5,3,7,101,18] 输出：4 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+var lengthOfLIS = function (nums) {
+    const len = nums.length
+    const dp = Array(len).fill(1)
+    for (let i = 1; i < len; i++) {//i结尾
+        for (let j = 0; j < i; j++) {// j开头
+            if (nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1) //记住吧，做错无数遍了
+        }
+    }
+    return Math.max(...dp)
+};
+// const res = lengthOfLIS([4, 10, 4, 3, 8, 9])
+
+
 // ------------------------------------------------------------------------------------dp多维------------------------------------------------------------------------------------
 // 120三角形最小路径和
-//
+// 给定一个三角形 triangle ，找出自顶向下的最小路径和。
+// 如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+var minimumTotal = function (triangle) {
+    const len = triangle.length
+    const dp = Array(len).fill(0).map(() => Array(len).fill(Infinity))
+    dp[0][0] = triangle[0][0]
+    for (let i = 1; i < len; i++) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            if (j === 0) {
+                dp[i][j] = dp[i - 1][j] + triangle[i][j]
+            } else {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
+
+            }
+        }
+    }
+
+
+    return Math.min(...dp[len - 1])
+
+
+};
+// const res = minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])
+
 // 64最小路径和
-//
+// 请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。每次只能向下或者向右移动一步。
+// 输入：grid = [[1,3,1],[1,5,1],[4,2,1]] 输出：7 解释：因为路径 1→3→1→1→1 的总和最小。
+var minPathSum1 = function (grid) {
+    const [m, n] = [grid.length, grid[0].length]
+    const dp = Array(m).fill(0).map(() => Array(n).fill(Infinity))
+    dp[0][0] = grid[0][0]
+    for (let i = 1; i < m; i++) {
+        dp[i][0] = dp[i - 1][0] + grid[i][0]
+    }
+    for (let j = 1; j < n; j++) {
+        dp[0][j] = dp[0][j - 1] + grid[0][j]
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        }
+    }
+    return dp[m - 1][n - 1]
+};
+const minPathSum = (grid) => {
+    const [m, n] = [grid.length, grid[0].length]
+    const dp = Array(m).fill(0)
+    dp[0] = grid[0][0]
+    for (let j = 1; j < n; j++) {
+        dp[j] = dp[j - 1] + grid[0][j]
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (j === 0) {
+                dp[j] += grid[i][j]
+            } else {
+                dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j]
+            }
+        }
+    }
+    return dp[n - 1]
+}
+
+// const res = minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]])
+
+
 // 63不同路径 II
-//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+// 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+var uniquePathsWithObstacles1 = function (obstacleGrid) {
+    // timeous
+    const [m, n] = [obstacleGrid.length, obstacleGrid[0].length]
+    let res = 0
+    const helper = (i = 0, j = 0) => {
+        if (i < 0 || i >= m || j < 0 || j >= n) return
+        if (obstacleGrid[i][j] === 1) return;
+        if (i === m - 1 && j === n - 1) return res += 1
+
+        i = i + 1
+        helper(i, j)
+        i = i - 1
+
+        j = j + 1
+        helper(i, j)
+        j = j - 1
+    }
+    helper()
+    return res
+};
+var uniquePathsWithObstacles = function (obstacleGrid) {
+    const [m, n] = [obstacleGrid.length, obstacleGrid[0].length]
+    const dp = Array(m).fill(0).map(() => Array(n).fill(0))// 到达点的路径个数
+    dp[0][0] = obstacleGrid[0][0] === 1 ? 0 : 1
+    for (let i = 1; i < m; i++) {
+        dp[i][0] = obstacleGrid[i][0] === 1 ? 0 : dp[i - 1][0]
+    }
+    for (let j = 1; j < n; j++) {
+        dp[0][j] = obstacleGrid[0][j] === 1 ? 0 : dp[0][j - 1]
+
+    }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (obstacleGrid[i][j] !== 1) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] //todo keypoint 我的路径数=左+上
+            }
+        }
+    }
+    return dp[m - 1][n - 1]
+}// const res = uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+
+
 // 5最长回文子串
-//
+var longestPalindrome = function (s) {
+    const len = s.length
+    if (len === 1) return s
+    if (len === 2) {
+        if (s[0] === s[1]) return s
+        return s[0]
+    }
+    const dp = Array(len).fill(0).map(() => Array(len).fill(false))
+    let start = 0, maxLength = 1
+    for (let i = 0; i < len; i++) dp[i][i] = true
+
+    for (let j = 1; j < len; j++) {
+        for (let i = 0; i < j; i++) {
+            if (s[i] === s[j]) {
+                if (j - i < 3 || dp[i + 1][j - 1]) {
+                    dp[i][j] = true
+                    if (j - i + 1 > maxLength) {
+                        start = i
+                        maxLength = j - i + 1
+                    }
+                }
+            }
+        }
+    }
+
+    return s.slice(start, start + maxLength)
+};
+// const res = longestPalindrome('ccc')
+
 // 97交错字符串
-//
+var isInterleave1 = function (s1, s2, s3) {
+    const l1 = s1.length, l2 = s2.length, l3 = s3.length
+    if (l1 + l2 !== l3) return false
+    let i = 0, j = 0, k = 0
+    let res = false
+    const helper = (i, j, k) => {
+        if (k === l3) return res = true
+        if (i < l1 && s1[i] === s3[k]) helper(i + 1, j, k + 1)
+        if (j < l2 && s2[j] === s3[k]) helper(i, j + 1, k + 1)
+    }
+    helper(i, j, k)
+    return res
+};
+const isInterleave = (s1, s2, s3) => {
+    const l1 = s1.length, l2 = s2.length, l3 = s3.length
+    if (l1 + l2 !== l3) return false
+    // i和j开头是否可以i+j的string
+    const dp = Array(l1 + 1).fill(0).map(() => Array(l2 + 1).fill(false))
+    dp[0][0] = true
+    for (let i = 0; i <= l1; i++) {
+        for (let j = 0; j <= l2; j++) {
+            if (i > 0 && s1[i - 1] === s3[i + j - 1]) dp[i][j] = dp[i - 1][j]
+            if (j > 0 && s2[j - 1] === s3[i + j - 1]) dp[i][j] = dp[i][j - 1] || dp[i][j]
+        }
+    }
+    return dp[l1][l2]
+}
+const res = isInterleave("aabcc", "dbbca", "aadbbcbcac")
 // 72编辑距离
 //
 // 123买卖股票的最佳时机 III
