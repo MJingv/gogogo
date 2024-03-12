fb34415fc3b9e31cb6b0f8074d4a570a85dbaaa8
MJingv <625619954@qq.com>
2024-03-12 16:44:50 +0800

    v8

---
+++ 0real/daily.js
@@ -0,0 +1,19 @@
+function Father(name) {
+    this.name = name || 'father'
+    this.money = 1000
+}
+
+
+Father.prototype.house = 10
+
+function Child(name) {
+    Father.call(this)
+    this.name = name || 'child'
+}
+
+Child.prototype = Object.create(Father.prototype)
+Child.prototype.constructor = Child
+
+const f = new Father('f')
+const c = new Child('c')
+console.log(c, c.house)
