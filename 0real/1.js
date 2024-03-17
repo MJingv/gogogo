8d69201757fca425922c3143a71dad480e72c88b
MJingv <625619954@qq.com>
2024-03-17 23:04:03 +0800

    rk

--- 0real/1.js
+++ 0real/1.js
@@ -413,5 +413,36 @@ var mergeTwoLists = function (list1, list2) {
     return dummy.next
 }
 
-const res = JSON.stringify(mergeTwoLists(l1, l3))
-console.log(res)
+// const res = JSON.stringify(mergeTwoLists(l1, l3))
+// console.log(res)
+async function foo() {
+    console.log('foo')
+}
+
+async function bar() {
+    console.log('bar start')
+    await foo()
+    console.log('bar end')
+}
+
+console.log('script start')
+setTimeout(function () {
+    console.log('setTimeout')
+}, 0)
+bar();
+new Promise(function (resolve) {
+    console.log('promise executor')
+    resolve();
+}).then(function () {
+    console.log('promise then')
+})
+console.log('script end')
+
+// script start
+// bar start
+// foo
+// promise executor
+// script end
+// bar end
+// promise then
+// setTimeout
