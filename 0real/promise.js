b2c6fa2db7c5a689ed4c99c60ff039ab702766c1
MJingv <625619954@qq.com>
2024-03-19 19:19:50 +0800

    concurrent

--- 0real/promise.js
+++ 0real/promise.js
@@ -37,3 +37,42 @@ const fn = (list, max) => {
 // const task = [() => t1(1), () => t1(2), () => t1(1)]
 // fn(task, 3).then(res => console.log(res)).catch(e => console.log(e))
 
+
+async function asyncPool(limit, array, iteratorFn) {
+    const ret = [];
+    const executing = [];
+    for (const item of array) {
+        const p = iteratorFn(item);
+        ret.push(p);
+        if (limit <= array.length) {
+            const e = p.then(() => {
+                console.log('正在运行' + executing.length)
+                executing.splice(executing.indexOf(e), 1)
+            });
+            executing.push(e);
+            if (executing.length >= limit) {
+                await Promise.race(executing);
+            }
+        }
+    }
+    return Promise.all(ret);
+}
+
+
+const timeout = (i) => {
+    console.log('开始' + i);
+
+    return new Promise((resolve) => setTimeout(() => {
+        resolve(i);
+        console.log('结束' + i);
+    }, 1000 + Math.random() * 1000));
+};
+
+let urls = Array(10).fill(0).map((v, i) => i)
+console.log(urls);
+(async () => {
+    const res = await asyncPool(3, urls, timeout);
+    console.log(res);
+})()
+
+
