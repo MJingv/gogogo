e9db87c3454c3d22a242c440caa8e4d002583d93
MJingv <625619954@qq.com>
2024-05-08 19:50:59 +0800

    merge

--- again/1.js
+++ again/1.js
@@ -14,15 +14,18 @@ const t1 = new TreeNode(1, t0, t3)
 
 
 class ListNode {
-    constructor(val, next) {
+    constructor(val, next = null) {
         this.val = val
-        this.next = next || null
+        this.next = next
     }
 }
 
-const l3 = new ListNode(3)
+const l5 = new ListNode(5)
+const l4 = new ListNode(4, l5)
+const l3 = new ListNode(3, l4)
 const l2 = new ListNode(2, l3)
 const l1 = new ListNode(1, l2)
+
 
 const reverse = (head) => {
     if (!head) return null
@@ -170,6 +173,27 @@ const mergeSort = (list = []) => {
 
 
 const list = [2, 4, 0, -99, 100, 2, 0, -1, 4, 222]
-const res = quickSort(list)
+// const res = quickSort(list)
 // const res = mergeSort(list)
+
+
+var mergeTwoLists = function (list1, list2) {
+    let p1 = list1, p2 = list2, dummy = new ListNode(-1)
+    let p = dummy
+    while (p1 && p2) {
+        if (p1.val < p2.val) {
+            p.next = p1
+            p1 = p1.next
+        } else {
+            p.next = p2
+            p2 = p2.next
+        }
+        p = p.next
+    }
+    if (p1) p.next = p1
+    if (p2) p.next = p2
+
+    return dummy.next
+}
+const res = JSON.stringify(mergeTwoLists(l1, l3))
 console.log(res)
